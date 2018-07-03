9a43b4b0e93c7475830ca9340107aa55400ddc82
Balaji Manoharan <balaji.mano3@gmail.com>
2018-07-03 11:29:38 -0500

    Fix: Fix Mirage in Directory so that all fixtures are loaded

--- packages/directory/tests/dummy/mirage/serializers/application.js
+++ packages/directory/tests/dummy/mirage/serializers/application.js
@@ -1,4 +1,8 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
+import { camelize } from '@ember/string';
 
 export default JSONAPISerializer.extend({
+  keyForAttribute: attr => camelize(attr),
+  keyForModel: attr => camelize(attr),
+  keyForRelationship: attr => camelize(attr)
 });
