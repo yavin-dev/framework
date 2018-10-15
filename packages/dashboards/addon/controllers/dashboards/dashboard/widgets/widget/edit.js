6512d6d355167a495a7eff308169cbd8ca4aacb6
Jon Kilroy <kilroy@oath.com>
2018-10-15 13:07:54 -0500

    Update: Clean up unused files

--- packages/dashboards/addon/controllers/dashboards/dashboard/widgets/widget/edit.js
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright 2017, Yahoo Holdings Inc.
- * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
- */
-import EditController from 'navi-reports/controllers/reports/report/edit';
-
-export default EditController.extend();
