9938e53674a1b8d5359256fbdf6a1aae0ef2a44f
cminick <cminick@oath.com>
2018-10-25 13:58:05 -0500

    Update: Strip down just what is needed for standalone

--- packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/beans/enums/DeliveryFrequency.kt
+++
@@ -1,8 +0,0 @@
-package com.yahoo.navi.ws.models.beans.enums
-
-enum class DeliveryFrequency private constructor(private val id: Int) {
-    day(4),
-    week(5),
-    month(6),
-    quarter(7)
-}
--- packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/permissions/PermissionExpressions.kt
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/permissions/PermissionExpressions.kt
@@ -2,21 +2,9 @@ package com.yahoo.navi.ws.models.permissions
 
 import com.yahoo.elide.security.checks.Check
 import com.yahoo.elide.security.checks.prefab.Role
-import com.yahoo.navi.ws.models.permissions.checks.SameUser
-import com.yahoo.navi.ws.models.permissions.checks.Author
-import com.yahoo.navi.ws.models.permissions.checks.DeliveryRuleHasSameUser
-import com.yahoo.navi.ws.models.permissions.checks.Editors
 
 object PermissionExpressions {
     var expressions: MutableMap<String, Class<out Check<out Any>>> = mutableMapOf(
-        "is the same user" to SameUser.AtCommit::class.java,
-        "is the same user now" to SameUser.AtOperation::class.java,
-        "is an author" to Author.AtCommitWithChangeSpec::class.java,
-        "is an author now" to Author.AtOperation::class.java,
-        "is an editor" to Editors.AtCommit::class.java,
-        "is an editor now" to Editors.AtOperation::class.java,
-        "delivery rule owned by same user" to DeliveryRuleHasSameUser.AtCommit::class.java,
-        "delivery rule owned by same user now" to DeliveryRuleHasSameUser.AtOperation::class.java,
         "everybody" to Role.ALL::class.java,
         "nobody" to Role.NONE::class.java
     )
