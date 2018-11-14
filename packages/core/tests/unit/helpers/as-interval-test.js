37f16af8bd9915a583e34378d019b8695abf9d3a
Matt Graham <mattgraham1995@gmail.com>
2018-11-14 15:55:59 -0600

    Update: Add date range filter value for dimensions with date values (#255)
    * Update: Add date range filter value for dimensions with date values
    
    * Update: Use only one date-range component
    
    * Update: Move some logic around
    
    * Update: Differentiate between datetime and dimension better
    
    * Fix: Forgot import
    
    * Update: Handle falsey values in as-interval

---
+++ packages/core/tests/unit/helpers/as-interval-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit';
+import Interval from 'navi-core/utils/classes/interval';
+import { asInterval } from 'navi-core/helpers/as-interval';
+
+module('Unit | Helper | as interval');
+
+test('interval object returned from interval object or interval string', function(assert) {
+  assert.expect(5);
+  const expectedInterval = Interval.parseFromStrings('2018-10-31', '2018-11-01');
+
+  assert.ok(
+    expectedInterval.isEqual(asInterval(['2018-10-31/2018-11-01'])),
+    'Interval string is turned into an interval class instance'
+  );
+  assert.ok(expectedInterval.isEqual(asInterval([expectedInterval])), 'Interval class instance is passed through');
+
+  assert.equal(asInterval(['']), '', 'Empty string is passed through');
+
+  assert.notOk(asInterval([undefined]), 'Undefined is passed through');
+
+  assert.notOk(asInterval([null]), 'Null value is passed through');
+});
