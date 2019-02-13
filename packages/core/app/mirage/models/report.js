6ccbf9538ffd8e7cee2f8152570746c3ecad11c6
Matt Graham <mgraham@yahoo-inc.com>
2019-02-13 17:44:39 -0600

    Update: Mirage models are not needed anymore

--- packages/core/app/mirage/models/report.js
+++
@@ -1,6 +0,0 @@
-import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
-
-export default Model.extend({
-  author: belongsTo('user', { inverse: 'reports' }),
-  deliveryRules: hasMany('delivery-rule', { inverse: 'deliveredItem' })
-});
