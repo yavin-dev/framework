011267bac88962773cc0b77546c124a1dd8c0cca
Matt Graham <mattgraham1995@gmail.com>
2019-02-14 15:31:33 -0600

    Update: Mirage models are not needed anymore (#312)

--- packages/core/app/mirage/models/dashboard.js
+++
@@ -1,7 +0,0 @@
-import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
-
-export default Model.extend({
-  author: belongsTo('user', { inverse: 'dashboards' }),
-  widgets: hasMany('dashboard-widget', { inverse: 'dashboard' }),
-  deliveryRules: hasMany('delivery-rule', { inverse: 'deliveredItem' })
-});
