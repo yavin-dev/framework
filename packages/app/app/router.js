66a95f9005844c294988f71596076e9225a2c749
Matt Graham <mgraham@yahoo-inc.com>
2019-02-20 13:47:38 -0600

    Fix: Tests in app

--- packages/app/app/router.js
+++ packages/app/app/router.js
@@ -17,20 +17,20 @@ Router.map(function() {
     this.route('my-data');
   });
 
-  this.route('dashboardCollections', function() {
-    this.route('collection', { path: '/:collectionId' });
+  this.route('dashboard-collections', function() {
+    this.route('collection', { path: '/:collection_id' });
   });
 
   this.route('dashboards', function() {
     this.route('new');
-    this.route('dashboard', { path: '/:dashboardId' }, function() {
+    this.route('dashboard', { path: '/:dashboard_id' }, function() {
       this.route('clone');
       this.route('view');
 
       this.route('widgets', function() {
         this.route('add');
         this.route('new');
-        this.route('widget', { path: '/:widgetId' }, function() {
+        this.route('widget', { path: '/:widget_id' }, function() {
           this.route('edit');
           this.route('invalid');
           this.route('view');
@@ -42,7 +42,7 @@ Router.map(function() {
 
   this.route('reports', function() {
     this.route('new');
-    this.route('report', { path: '/:reportId' }, function() {
+    this.route('report', { path: '/:report_id' }, function() {
       this.route('edit');
       this.route('invalid');
       this.route('view');
@@ -53,7 +53,7 @@ Router.map(function() {
 
   this.route('beta', function() {
     this.route('reports', function() {
-      this.route('report', { path: '/:reportId' }, function() {
+      this.route('report', { path: '/:report_id' }, function() {
         this.route('view');
       });
     });
