2342dff79240abce4452fde1c18d8da18eb2ba58
Daniel Levinson <daniellevinson@users.noreply.github.com>
2019-03-07 14:01:03 -0800

    Fix: Sorting in directory (#333)

--- packages/core/tests/unit/models/dashboard-test.js
+++ packages/core/tests/unit/models/dashboard-test.js
@@ -48,8 +48,8 @@ test('Retrieving Records', function(assert) {
         {
           title: 'Dashboard 2',
           author: 'navi_user',
-          createdOn: '2016-01-01 00:00:00.000',
-          updatedOn: '2016-01-01 00:00:00.000',
+          createdOn: '2016-02-01 00:00:00.000',
+          updatedOn: '2016-02-01 00:00:00.000',
           presentation: {
             version: 1,
             layout: [
