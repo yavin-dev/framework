e10366f72abd9bd9550c5442d8bf661d4d764cb4
Matt Graham <mgraham@yahoo-inc.com>
2019-04-02 11:20:23 -0500

    Update: Use Regex and add tests

--- packages/core/test-support/helpers/contains-helpers.js
+++ packages/core/test-support/helpers/contains-helpers.js
@@ -31,11 +31,11 @@ export function linkContains(text) {
 /**
  * This function allows us to keep using the jquery notation for :contains without using jquery
  *
+ * Need quotes around content with parentheses -- e.g. class:contains("(content)")
+ *
+ *
  * Example:
  * Jquery: $(.class-thing:contains(blah) .metric .test:contains(other))
- *
- * findByContains:
- * [...findByContains('.class-thing', 'blah').querySelectorAll('.metric .test')].find(el => el.innerText.trim().includes('other'))
  *
  * Non-Jquery Equivalent: [
  *    ...[
@@ -53,36 +53,22 @@ export function linkContains(text) {
  * @returns {HTMLElement} - Element based on text it (or its parent) contains
  */
 export function findContains(selector, baseElement) {
-  let nativeSelector,
-    parentElement = baseElement || document;
+  let parentElement = baseElement || document;
 
   if (selector.includes(':contains(')) {
-    let splits = selector.split(':contains(');
-    let firstSelector = splits.shift(); //Selector without the :contains operator
-    let restOfSelector = splits.join(':contains('); //We only want to split on the first instance of :contains(, so join the rest back together if there were more
-    let endOfContentIndex = findEndOfContentIndex(restOfSelector);
-    let content = restOfSelector.substring(0, endOfContentIndex);
-    let nextSelector = restOfSelector.substring(endOfContentIndex + 1).trim();
-    let filteredElement = [...parentElement.querySelectorAll(firstSelector)].find(el =>
-      el.innerText.trim().includes(content)
-    );
+    let regex = /^(.*?):contains\((["']?)(.*?)\2\)(.*)$/;
+    let matches = regex.exec(selector);
+    let firstSelector = matches[1];
+    let content = matches[3];
+    let nextSelector = matches[4];
+    let filteredElement = [...parentElement.querySelectorAll(firstSelector)].find(el => el.innerText.includes(content));
 
     /**
      * If the :contains was used at the very end of the selector, return the element we found here
      * Else recursively find element from rest of selector using the found element as the base
      */
     return nextSelector ? findContains(nextSelector, filteredElement) : filteredElement;
-  } else {
-    nativeSelector = selector;
   }
 
-  return parentElement.querySelector(nativeSelector);
-}
-
-/**
- * @param {String} str - String with the content ended with a ) and possibly another selector following that
- * @returns {Number} Index of the ) character that closes the :contains( expression that precedes the input string
- */
-function findEndOfContentIndex(str) {
-  return str.includes(':contains(') ? str.lastIndexOf(')', str.indexOf(':contains')) : str.lastIndexOf(')');
+  return parentElement.querySelector(selector);
 }
