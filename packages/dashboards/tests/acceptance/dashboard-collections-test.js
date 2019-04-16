f6bbfe176c0c68ee98eb6b761e6e71255635d664
jkusa <jkilroy@gmail.com>
2019-04-16 02:53:03 +0000

    Fix: Changed to dashboard-collections

--- packages/dashboards/tests/acceptance/dashboard-collections-test.js
+++ packages/dashboards/tests/acceptance/dashboard-collections-test.js
@@ -1,11 +1,8 @@
-import { findAll, visit } from '@ember/test-helpers';
-import Ember from 'ember';
+import { visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { Response } from 'ember-cli-mirage';
-
-let OriginalLoggerError, OriginalTestAdapterException;
 
 module('Acceptance | Dashboard Collections', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,42 +11,30 @@ module('Acceptance | Dashboard Collections', function(hooks) {
   test('dashobard-collection success', async function(assert) {
     assert.expect(2);
 
-    await visit('/dashboardCollections/1');
-    assert.notOk(!!findAll('.error').length, 'Error message not present when route is successfully loaded');
+    await visit('/dashboard-collections/1');
+    assert.dom('.error').doesNotExist('Error message not present when route is successfully loaded');
 
-    assert.ok(
-      !!findAll('.navi-collection').length,
-      'the dashboard collection component is rendered when route is successfully loaded'
-    );
+    assert
+      .dom('.navi-collection')
+      .exists('the dashboard collection component is rendered when route is successfully loaded');
   });
 
   test('dashboard-collection error', async function(assert) {
     assert.expect(2);
 
-    // Allow testing of errors - https://github.com/emberjs/ember.js/issues/11469
-    OriginalLoggerError = Ember.Logger.error;
-    OriginalTestAdapterException = Ember.Test.adapter.exception;
-    Ember.Logger.error = function() {};
-    Ember.Test.adapter.exception = function() {};
+    server.get('/dashboardCollections/:id', () => new Response(500));
 
-    server.get('/dashboardCollections/:id', () => {
-      return new Response(500);
-    });
+    await visit('/dashboard-collections/1');
+    assert.dom('.error').exists('Error message is present when route encounters an error');
 
-    await visit('/dashboardCollections/1');
-    assert.ok(!!findAll('.error').length, 'Error message is present when route encounters an error');
-
-    assert.notOk(!!findAll('.navi-collection').length, 'Navi dashboard collection component is not rendered');
-
-    Ember.Logger.error = OriginalLoggerError;
-    Ember.Test.adapter.exception = OriginalTestAdapterException;
+    assert.dom('.navi-collection').doesNotExist('Navi dashboard collection component is not rendered');
   });
 
   test('dashboard-collection loading', async function(assert) {
     assert.expect(1);
 
-    await visit('/dashboardCollections/loading');
+    await visit('/dashboard-collections/loading');
 
-    assert.ok(!!findAll('.loader-container').length, 'Loader is present when visiting loading route');
+    assert.dom('.loader-container').exists('Loader is present when visiting loading route');
   });
 });
