05e2adb473e9a041e1a7bbb5d709754f5d7b5ffd
Alex Aralis <alex.aralis@oath.com>
2019-04-18 12:29:11 -0500

    Update: Inject invalid filter errors into JSON API response.

--- packages/dashboards/addon/services/dashboard-data.js
+++ packages/dashboards/addon/services/dashboard-data.js
@@ -2,23 +2,26 @@
  * Copyright 2017, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
-import Ember from 'ember';
-import DS from 'ember-data';
-import _ from 'lodash';
+import { A as arr } from '@ember/array';
+import { get, getWithDefault } from '@ember/object';
+import { inject } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+import { merge, flow } from 'lodash';
 import { v1 } from 'ember-uuid';
+import DS from 'ember-data';
+import RSVP from 'rsvp';
+import Service from '@ember/service';
 
-const { get } = Ember;
-
-export default Ember.Service.extend({
+export default Service.extend({
   /**
    * @property {Ember.Service} bardFacts
    */
-  bardFacts: Ember.inject.service(),
+  bardFacts: inject(),
 
   /**
    * @property {Ember.Service} store
    */
-  store: Ember.inject.service(),
+  store: inject(),
 
   /**
    * @property {Object} widgetOptions - options for the fact request
@@ -47,26 +50,36 @@ export default Ember.Service.extend({
    * @returns {Object} hash of widget id to data promise array
    */
   fetchDataForWidgets(dashboardId, widgets = [], decorators = [], options = {}) {
-    let result = {},
-      uuid = v1();
+    const result = {};
+    const uuid = v1();
 
     // Construct hash of widget id to data
     widgets.forEach(widget => {
-      let widgetId = get(widget, 'id'),
-        widgetDataPromises = get(widget, 'requests').map(request => {
-          //construct custom header for each widget with uuid
-          options.customHeaders = {
-            uiView: `dashboard.${dashboardId}.${uuid}.${widgetId}`
-          };
+      const widgetId = get(widget, 'id');
+      const widgetDataPromises = get(widget, 'requests').map(request => {
+        //construct custom header for each widget with uuid
+        options.customHeaders = {
+          uiView: `dashboard.${dashboardId}.${uuid}.${widgetId}`
+        };
 
-          request = this._applyFilters(widget, request);
-          request = this._decorate(decorators, request.serialize());
+        request = this._applyFilters(widget, request);
+        request = this._decorate(decorators, request.serialize());
 
-          return this._fetch(request, options);
+        const filterErrors = this._getFilterErrors(widget);
+
+        return this._fetch(request, options).then(result => {
+          const serverErrors = getWithDefault(result, 'response.errors', []);
+
+          return merge({}, result, {
+            response: {
+              errors: [...serverErrors, ...filterErrors]
+            }
+          });
         });
+      });
 
       result[get(widget, 'id')] = DS.PromiseArray.create({
-        promise: Ember.RSVP.all(widgetDataPromises).then(Ember.A) // PromiseArray expects an Ember array returned
+        promise: RSVP.all(widgetDataPromises).then(arr) // PromiseArray expects an Ember array returned
       });
     });
 
@@ -81,10 +94,10 @@ export default Ember.Service.extend({
    * @returns {Object} transformed version of request
    */
   _decorate(decorators, request) {
-    if (Ember.isEmpty(decorators)) {
+    if (isEmpty(decorators)) {
       return request;
     } else {
-      return _.flow(...decorators)(request);
+      return flow(...decorators)(request);
     }
   },
 
@@ -95,14 +108,68 @@ export default Ember.Service.extend({
    *
    * @param {Object} widget
    * @param {Object} request
+   * @returns {Object}
    */
   _applyFilters(widget, request) {
     const requestClone = request.clone();
-    const filters = get(widget, 'dashboard.filters');
 
-    filters.forEach(filter => requestClone.addFilter(filter));
+    this._getValidGlobalFilters(widget).forEach(filter => requestClone.addFilter(filter));
 
     return requestClone;
+  },
+
+  /**
+   * Finds the invalid global filters for a
+   * widget and returns them.
+   *
+   * @param {Object} widget
+   * @returns {Array<Object>}
+   */
+  _getInvalidGlobalFilters(widget) {
+    const filters = getWithDefault(widget, 'dashboard.filters', []);
+
+    return filters.filter(filter => !this._isFilterValid(widget, filter));
+  },
+
+  /**
+   *
+   * @param {Object} widget
+   */
+  _getFilterErrors(widget) {
+    const invalidFilters = this._getInvalidGlobalFilters(widget);
+
+    return invalidFilters.map(filter => ({
+      detail: `"${get(filter, 'dimension.name')}" is not a dimension in the "${get(
+        widget,
+        'request.logicalTable.table.name'
+      )}" table.`,
+      title: 'Invalid Filter'
+    }));
+  },
+
+  /**
+   * Finds the valid global filters for a
+   * widget and returns them.
+   *
+   * @param {Object} widget
+   * @returns {Array<Object>}
+   */
+  _getValidGlobalFilters(widget) {
+    const filters = getWithDefault(widget, 'dashboard.filters', []);
+
+    return filters.filter(filter => this._isFilterValid(widget, filter));
+  },
+
+  /**
+   *
+   * @param {Object} widget
+   * @param {Object} filter
+   * @returns {Boolean}
+   */
+  _isFilterValid(widget, filter) {
+    const validDimensions = []; // get(widget, 'request.logicalTable.timeGrain.dimensionIds');
+
+    return validDimensions.includes(get(filter, 'dimension.name'));
   },
 
   /**
