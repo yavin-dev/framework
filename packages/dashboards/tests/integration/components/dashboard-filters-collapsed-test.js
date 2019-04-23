81fb1b38410d092da6c33e1e41b3b975325a9fdc
Alex Aralis <alex.aralis@oath.com>
2019-04-23 16:35:25 -0500

    Update: Test collapsed filters component.

--- packages/dashboards/tests/integration/components/dashboard-filters-collapsed-test.js
+++ packages/dashboards/tests/integration/components/dashboard-filters-collapsed-test.js
@@ -5,13 +5,64 @@ moduleForComponent('dashboard-filters-collapsed', 'Integration | Component | das
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders empty', function(assert) {
   this.render(hbs`{{dashboard-filters-collapsed}}`);
 
   assert.equal(
     this.$()
       .text()
       .trim(),
-    'Settings'
+    'Settings',
+    'When no filters are provided, only "Settings" is rendered'
+  );
+});
+
+test('it renders all filters attached to the dashboard', function(assert) {
+  this.dashboard = {
+    filters: [
+      {
+        dimension: {
+          name: 'property',
+          longName: 'Property'
+        },
+        operator: 'in',
+        field: 'key',
+        rawValues: ['property|4', 'property|7', 'property|9'],
+        values: [
+          { key: 'property|7', id: 'property|4', description: 'Something' },
+          { key: 'property|4', id: 'property|7', description: 'ValueDesc' }
+        ]
+      },
+      {
+        dimension: {
+          name: 'fish',
+          longName: 'Fish'
+        },
+        operator: 'contains',
+        field: 'id',
+        rawValues: ['1', '2'],
+        values: [{ id: '1', description: 'Something' }, { id: '2', description: 'ValueDesc' }]
+      },
+      {
+        dimension: {
+          name: 'dog',
+          longName: 'Dog'
+        },
+        operator: 'notin',
+        field: 'id',
+        rawValues: ['1', '2'],
+        values: [{ id: '2', description: 'ValueDesc' }]
+      }
+    ]
+  };
+  this.render(hbs`{{dashboard-filters-collapsed dashboard=dashboard}}`);
+
+  assert.equal(
+    this.$()
+      .text()
+      .trim()
+      .replace(/\s+/g, ' '),
+    'Settings Property equals ValueDesc (property|4), Something (property|7), property|9 Fish contains Something (1), ValueDesc (2) Dog not equals 1, ValueDesc (2)',
+    'All filters are correctly displayed'
   );
 });
