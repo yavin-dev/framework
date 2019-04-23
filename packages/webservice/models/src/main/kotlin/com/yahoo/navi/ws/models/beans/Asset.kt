d6ffc220753b351ec1255557410205ab624faab0
cminick <cminick@oath.com>
2019-04-23 11:36:41 -0500

    Update: Report initial beans

---
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/beans/Asset.kt
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2019, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+package com.yahoo.navi.ws.models.beans
+
+import com.yahoo.elide.annotation.ComputedAttribute
+import com.yahoo.elide.annotation.ReadPermission
+import com.yahoo.elide.annotation.UpdatePermission
+import com.yahoo.navi.ws.models.utils.FormatDate
+import org.hibernate.annotations.Generated
+import org.hibernate.annotations.GenerationTime
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Inheritance
+import javax.persistence.InheritanceType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.OneToOne
+import javax.persistence.PrimaryKeyJoinColumn
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+import javax.persistence.Transient
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+abstract class Asset {
+    @get:Id
+    @get:GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0
+
+    var title: String? = null
+
+    @get:Generated(GenerationTime.INSERT)
+    @get:Column(updatable = false, insertable = false, columnDefinition = "timestamp default current_timestamp")
+    @get:Temporal(TemporalType.TIMESTAMP)
+    @get:UpdatePermission(expression = "nobody")
+    var createdOn: Date? = null
+
+    @get:Generated(GenerationTime.ALWAYS)
+    @get:Column(updatable = false, insertable = false, columnDefinition = "timestamp default current_timestamp")
+    @get:Temporal(TemporalType.TIMESTAMP)
+    @get:UpdatePermission(expression = "nobody")
+    var updatedOn: Date? = null
+}
---
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/permissions/checks/Author.kt
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2019, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+package com.yahoo.navi.ws.models.permissions.checks
+
+import com.yahoo.elide.security.ChangeSpec
+import com.yahoo.elide.security.RequestScope
+import com.yahoo.elide.security.checks.CommitCheck
+import com.yahoo.elide.security.checks.OperationCheck
+import com.yahoo.navi.ws.models.beans.HasAuthor
+import java.security.Principal
+import java.util.Optional
+import kotlin.collections.Collection
+
+object Author {
+
+    /**
+     * @param record user
+     * @param requestScope Elide Resource
+     * @param changeSpec Elide Resource
+     * @return true if given record id matches user's id
+     */
+    private fun check(record: HasAuthor, requestScope: RequestScope): Boolean {
+        val user = requestScope.user!!.opaqueUser as Principal
+        val recordId = record.author!!.id
+        val userId = user.name
+
+        return recordId == userId
+    }
+
+    /**
+     * Checks at the operation level
+     */
+    class AtOperation : OperationCheck<HasAuthor>() {
+
+        /**
+         * @param record user
+         * @param requestScope Elide Resource
+         * @param changeSpec Elide Resource
+         * @return true if given record id matches user's id
+         */
+        override fun ok(record: HasAuthor, requestScope: RequestScope, changeSpec: Optional<ChangeSpec>): Boolean {
+            return check(record, requestScope)
+        }
+    }
+
+    /**
+     * Checks at the commit level
+     */
+    class AtCommitWithChangeSpec : CommitCheck<HasAuthor>() {
+
+        /**
+         * @param record user
+         * @param requestScope Elide Resource
+         * @param changeSpec Elide Resource
+         * @return
+         */
+        override fun ok(record: HasAuthor, requestScope: RequestScope, changeSpec: Optional<ChangeSpec>): Boolean {
+            if(!changeSpec.isPresent()) {
+                return check(record, requestScope)
+            }
+
+            val original: Any? = changeSpec.get().original
+            val modified: Any? = changeSpec.get().modified
+
+            val user = requestScope.user!!.opaqueUser as Principal
+
+            if(original is Collection<*> && modified is Collection<*>
+                && original.all { orig -> orig is HasAuthor} && modified.all { mod -> mod is HasAuthor}) {
+                val records: List<HasAuthor> = modified.subtract(original).map{ it -> it as HasAuthor}
+                return records.all { it -> it.author!!.id == user.name }
+            } else {
+                val resource: Any? = changeSpec.get().resource?.`object`
+                if(resource != null) {
+                    return check(resource as HasAuthor, requestScope)
+                }
+            }
+            return true
+        }
+    }
+}
