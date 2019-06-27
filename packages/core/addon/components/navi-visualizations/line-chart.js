f2fd7ac6c62de2114e00a5ed92cf7b99d81e9b61
Kevin Hinterlong <hinterlong@verizonmedia.com>
2019-06-27 13:36:30 -0500

    Update: Position of x labels for year chart grain

--- packages/core/addon/components/navi-visualizations/line-chart.js
+++ packages/core/addon/components/navi-visualizations/line-chart.js
@@ -22,6 +22,7 @@ import { inject as service } from '@ember/service';
 import layout from '../../templates/components/navi-visualizations/line-chart';
 import numeral from 'numeral';
 import merge from 'lodash/merge';
+import moment from 'moment';
 import { run } from '@ember/runloop';
 
 const DEFAULT_OPTIONS = {
@@ -129,7 +130,8 @@ export default Component.extend({
       { point },
       { axis: { x: { type: 'category' } } }, // Override old 'timeseries' config saved in db
       get(this, 'yAxisLabelConfig'),
-      get(this, 'yAxisDataFormat')
+      get(this, 'yAxisDataFormat'),
+      get(this, 'xAxisTickValues')
     );
   }),
 
@@ -257,6 +259,45 @@ export default Component.extend({
      * Use the factory that has been registered instead of an anonymous component.
      */
     return owner.factoryFor(registryEntry);
+  }),
+
+  xAxisTickValues: computed('model.firstObject', 'seriesConfig', function() {
+    const chartGrain = get(this, 'seriesConfig.config.timeGrain');
+    if (chartGrain !== 'year') {
+      return {};
+    }
+    const requestGrain =
+      get(this, 'model.firstObject.request.logicalTable.timeGrain.name') ||
+      get(this, 'model.firstObject.request.logicalTable.timeGrain');
+
+    const dayValues = [];
+    for (let i = 0; i < 12; i++) {
+      dayValues.push(
+        moment()
+          .startOf('year')
+          .month(i)
+          .dayOfYear()
+      );
+    }
+
+    const values = {
+      day: dayValues,
+      // week.by.year in date-time is hardcoded to YEAR_WITH_53_ISOWEEKS (2015)
+      week: [1, 5, 9, 13, 18, 22, 26, 31, 35, 39, 44, 48],
+      month: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    }[requestGrain];
+
+    return {
+      axis: {
+        x: {
+          tick: {
+            values,
+            fit: !values,
+            culling: !values
+          }
+        }
+      }
+    };
   }),
 
   /**
