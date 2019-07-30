eeb585f100f1ebab098e7fe8604a7256436ab1c4
Chad <cminick@verizonmedia.com>
2019-07-30 11:12:48 -0500

    Fix: Dashboard creation for ember 2.x (#458)

--- packages/dashboards/addon/routes/dashboards/new.js
+++ packages/dashboards/addon/routes/dashboards/new.js
@@ -30,8 +30,13 @@ export default Route.extend({
    * @override
    * @returns {DS.Model} route model
    */
-  model() {
-    const { queryParams = {} } = this.get('router.currentRoute');
+  model(_, transition) {
+    const queryParams =
+      (transition &&
+        (transition.queryParams || //Ember2.x support
+          (transition.to && transition.to.queryParams))) ||
+      {};
+
     return this._newModel(queryParams.title);
   },
 
@@ -42,8 +47,12 @@ export default Route.extend({
    * @param dashboard - resolved dashboard model
    * @override
    */
-  afterModel(dashboard) {
-    const { queryParams } = this.get('router.currentRoute');
+  afterModel(dashboard, transition) {
+    const queryParams =
+      (transition &&
+        (transition.queryParams || //Ember2.x support
+          (transition.to && transition.to.queryParams))) ||
+      {};
 
     // If an initial widget was given in the query params, create it
     if (queryParams.unsavedWidgetId) {
