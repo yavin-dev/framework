466e253c807d3ce0f1aa79591715ba6cb71e859c
Kevin Hinterlong <hinterlong@verizonmedia.com>
2019-08-02 09:29:03 -0500

    Fix: Fetch gh-pages branch before checking it out

--- scripts/ci-publish.sh
+++ scripts/ci-publish.sh
@@ -16,6 +16,7 @@ cd packages/app
 export BUILD_NAVI_DEMO=true
 COMMIT=$(git rev-parse --short HEAD)
 git remote add ssh-origin git@github.com:yahoo/navi.git
+git fetch ssh-origin gh-pages
 npx ember github-pages:commit --message "Deploy gh-pages from $COMMIT}" --destination ../../
 ssh-agent sh -c "ssh-add $TRAVIS_BUILD_DIR/travis-secrets/deploy_rsa;
   git push ssh-origin gh-pages:gh-pages || echo Failed to push gh-pages update"
