4696d9518b58cf40c25eb8577ed32b8944320a5f
Jon Kilroy <jkilroy@gmail.com>
2019-09-12 11:43:22 -0500

    Update: Line chart config styles (#505)
    - Selects are no longer rounded
    - Text color and font are aligned
    - Stack chart series is now always displayed when stacked is enabled

--- packages/core/tests/integration/components/chart-series-config-test.js
+++ packages/core/tests/integration/components/chart-series-config-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, findAll, click } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupMock, teardownMock } from '../../helpers/mirage-helper';
 
@@ -15,35 +15,43 @@ const TEMPLATE = hbs`
 `;
 
 const SERIES_CONFIG = {
-  metrics: [{
-    metric: "adClicks",
-    parameters: {}
-  }, {
-    metric: "revenue",
-    parameters: {
-      currency: "CAD",
-      as: "m1"
+  metrics: [
+    {
+      metric: 'adClicks',
+      parameters: {}
+    },
+    {
+      metric: 'revenue',
+      parameters: {
+        currency: 'CAD',
+        as: 'm1'
+      }
+    },
+    {
+      metric: 'revenue',
+      parameters: {
+        currency: 'EUR',
+        as: 'm2'
+      }
     }
-  }, {
-    metric: "revenue",
-    parameters: {
-      currency: "EUR",
-      as: "m2"
+  ],
+  dimensions: [
+    {
+      name: 'Property 1'
+    },
+    {
+      name: 'Property 2'
+    },
+    {
+      name: 'Property 3'
     }
-  }],
-  dimensions: [{
-    name: "Property 1"
-  }, {
-    name: "Property 2"
-  }, {
-    name: "Property 3"
-  }]
+  ]
 };
 
-module('Integration | Component | chart series config', function (hooks) {
+module('Integration | Component | chart series config', function(hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
+  hooks.beforeEach(function() {
     setupMock();
 
     this.setProperties({
@@ -56,68 +64,31 @@ module('Integration | Component | chart series config', function (hooks) {
     return MetadataService.loadMetadata();
   });
 
-  hooks.afterEach(function () {
+  hooks.afterEach(function() {
     teardownMock();
   });
 
-  test('Component renders', async function (assert) {
+  test('Component renders', async function(assert) {
     assert.expect(1);
 
     await render(TEMPLATE);
 
-    assert
-      .dom('.line-chart-config__series-config')
-      .exists('The chart series config component is rendered');
+    assert.dom('.line-chart-config__series-config').exists('The chart series config component is rendered');
   });
 
-  test('Component is collapsible', async function (assert) {
-    assert.expect(6);
-
-    await render(TEMPLATE);
-
-    assert
-      .dom('.line-chart-config__series-config__header .fa-angle-right')
-      .exists('The chart series config component is closed by default');
-
-    assert
-      .dom('.line-chart-config__series-config__content')
-      .isNotVisible('The chart series config component is not visible by default');
-
-    await click('.line-chart-config__series-config__header');
-
-    assert
-      .dom('.line-chart-config__series-config__header .fa-angle-down')
-      .exists('The chart series config component is open after clicking header');
-
-    assert
-      .dom('.line-chart-config__series-config__content')
-      .isVisible('The chart series config component is visible after clicking header');
-
-    await click('.line-chart-config__series-config__header');
-
-    assert
-      .dom('.line-chart-config__series-config__header .fa-angle-right')
-      .exists('The chart series config component is closed after clicking header twice');
-
-    assert
-      .dom('.line-chart-config__series-config__content')
-      .isNotVisible('The chart series config component is not visible after clicking header twice');
-  });
-
-  test('Component renders dimensions correctly', async function (assert) {
+  test('Component renders dimensions correctly', async function(assert) {
     assert.expect(1);
-
-    this.set('isOpen', true);
 
     await render(TEMPLATE);
 
     assert.deepEqual(
       findAll('.line-chart-config__series-config__item__content').map(el => el.textContent.trim()),
       ['Property 1', 'Property 2', 'Property 3'],
-      'Component renders dimension names correctly');
+      'Component renders dimension names correctly'
+    );
   });
 
-  test('Component renders formatted metrics correctly', async function (assert) {
+  test('Component renders formatted metrics correctly', async function(assert) {
     assert.expect(1);
 
     this.setProperties({
@@ -130,6 +101,7 @@ module('Integration | Component | chart series config', function (hooks) {
     assert.deepEqual(
       findAll('.line-chart-config__series-config__item__content').map(el => el.textContent.trim()),
       ['Ad Clicks', 'Revenue (CAD)', 'Revenue (EUR)'],
-      'Component renders metric names correctly');
+      'Component renders metric names correctly'
+    );
   });
 });
