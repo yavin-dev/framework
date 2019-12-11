808e08bdc745a37a1b2a1deb692ab76a330048a6
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2019-12-11 11:06:41 -0600

    Update: Disable column reordering inside navi widget (#613)
    * Update: Disable column reordering inside navi widget
    
    * Update: Fix tests
    
    * Update: PR Comments
    
    - oops
    
    * Update: Syntax and PR comments

--- packages/core/tests/test-helper.js
+++ packages/core/tests/test-helper.js
@@ -2,8 +2,6 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import 'ember-sortable/helpers/drag';
-import 'ember-sortable/helpers/reorder';
 
 const application = Application.create(config.APP);
 
