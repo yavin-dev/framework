6c4c6eb77331843c3aeed338cac4ad7f3715e29d
Matt Graham <mgraham@yahoo-inc.com>
2020-01-07 14:07:01 -0600

    Update: Add test for visualization-selector

--- packages/reports/tests/integration/components/visualization-selector-test.js
+++ packages/reports/tests/integration/components/visualization-selector-test.js
@@ -1,26 +1,56 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | visualization-selector', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    assert.expect(3);
 
-    await render(hbs`<VisualizationSelector />`);
+    this.report = {
+      visualization: {
+        type: 'line-chart'
+      }
+    };
+    this.validVisualizations = [
+      {
+        name: 'line-chart',
+        niceName: 'Line Chart',
+        icon: 'line-chart'
+      },
+      {
+        name: 'bar-chart',
+        niceName: 'Bar Chart',
+        icon: 'bar-chart'
+      },
+      {
+        name: 'table',
+        niceName: 'Data Table',
+        icon: 'table'
+      }
+    ];
+    this.onVisualizationTypeUpdate = name => {
+      assert.equal(name, 'request-preview', 'The clicked visualization type is sent to the update type action');
+    };
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`<VisualizationSelector 
+      @report={{report}}
+      @validVisualizations={{validVisualizations}}
+      @onVisualizationTypeUpdate={{action onVisualizationTypeUpdate}}
+    />`);
 
-    // Template block usage:
-    await render(hbs`
-      <VisualizationSelector>
-        template block text
-      </VisualizationSelector>
-    `);
+    assert.deepEqual(
+      findAll('.visualization-selector__option').map(el => el.title),
+      ['Request Preview', 'Line Chart', 'Bar Chart', 'Data Table'],
+      'Each valid visualization and the request preview are included '
+    );
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert
+      .dom('.visualization-selector__option--is-active')
+      .hasAttribute('title', 'Line Chart', 'The visualization type in the report is shown as the selected option');
+
+    await click('.visualization-selector__option[title="Request Preview"]');
   });
 });
