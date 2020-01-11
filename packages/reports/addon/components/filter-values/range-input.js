8decb5f1cd8a71fa2e3fef35d4ea61b9dca6276a
Daniel Levinson <levinson.dl@gmail.com>
2020-01-10 17:04:56 -0800

    Fix: Metric filter input

--- packages/reports/addon/components/filter-values/range-input.js
+++ packages/reports/addon/components/filter-values/range-input.js
@@ -1,57 +1,55 @@
 /**
- * Copyright 2017, Yahoo Holdings Inc.
+ * Copyright 2020, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
- *   {{filter-values/range-input
- *       filter=filter
- *       onUpdateFilter=(action 'update')
- *       lowPlaceholder=lowPlaceholder
- *       highPlaceholder=highPlaceholder
- *   }}
+ *   <FilterValues::RangeInput
+ *     @filter={{filter}}
+ *     @onUpdateFilter={{action "update"}}
+ *     @lowPlaceholder={{lowPlaceholder}}
+ *     @highPlaceholder={{highPlaceholder}}
+ *   />
  */
 
 import Component from '@ember/component';
-import { get } from '@ember/object';
+import { action, get } from '@ember/object';
 import layout from '../../templates/components/filter-values/range-input';
+import { layout as templateLayout, tagName } from '@ember-decorators/component';
 
-export default Component.extend({
-  layout,
-
-  /**
-   * @property {Array} classNames
-   */
-  classNames: ['filter-values--range-input'],
-
+@tagName('')
+@templateLayout(layout)
+class RangeInput extends Component {
   /**
    * @property {String} lowPlaceholder
    */
-  lowPlaceholder: 'Low value',
+  lowPlaceholder = 'Low value';
 
   /**
    * @property {String} highPlaceholder
    */
-  highPlaceholder: 'High value',
+  highPlaceholder = 'High value';
 
-  actions: {
-    /**
-     * @action setLowValue
-     * @param {String} value - first value to be set in filter
-     */
-    setLowValue(value) {
-      this.onUpdateFilter({
-        values: [value, get(this, 'filter.values.lastObject')]
-      });
-    },
-
-    /**
-     * @action setHighValue
-     * @param {String} value - last value to be set in filter
-     */
-    setHighValue(value) {
-      this.onUpdateFilter({
-        values: [get(this, 'filter.values.firstObject'), value]
-      });
-    }
+  /**
+   * @action setLowValue
+   * @param {String} value - first value to be set in filter
+   */
+  @action
+  setLowValue(value) {
+    this.onUpdateFilter({
+      values: [value, get(this, 'filter.values.lastObject')]
+    });
   }
-});
+
+  /**
+   * @action setHighValue
+   * @param {String} value - last value to be set in filter
+   */
+  @action
+  setHighValue(value) {
+    this.onUpdateFilter({
+      values: [get(this, 'filter.values.firstObject'), value]
+    });
+  }
+}
+
+export default RangeInput;
