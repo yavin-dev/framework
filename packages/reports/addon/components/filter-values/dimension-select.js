e251a76ee473af685e9210a2be196122b59cc2d5
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2020-01-14 10:48:07 -0600

    Update: Fix dimension select search behavior (#629)
    * Update: Fix dimension select search behavior
    
    - only show options while not searching
    
    * Update: PR Comments
    
    * Apply suggestions from code review
    
    Co-Authored-By: Daniel Levinson <daniellevinson@users.noreply.github.com>
    
    Co-authored-by: Daniel Levinson <daniellevinson@users.noreply.github.com>

--- packages/reports/addon/components/filter-values/dimension-select.js
+++ packages/reports/addon/components/filter-values/dimension-select.js
@@ -1,58 +1,63 @@
 /**
- * Copyright 2019, Yahoo Holdings Inc.
+ * Copyright 2020, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
- *   {{filter-values/dimension-select
- *       filter=filter
- *       onUpdateFilter=(action 'update')
- *   }}
+ *   <FilterValues::DimensionSelect
+ *       @filter={{filter}}
+ *       @onUpdateFilter={{action "update"}}
+ *   />
  */
 import config from 'ember-get-config';
 import { featureFlag } from 'navi-core/helpers/feature-flag';
-import { readOnly } from '@ember/object/computed';
 import { debounce } from '@ember/runloop';
 import { A } from '@ember/array';
 import { resolve, Promise } from 'rsvp';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { get, computed } from '@ember/object';
+import { get, set, computed, action } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
 import layout from '../../templates/components/filter-values/dimension-select';
+import { layout as templateLayout, tagName } from '@ember-decorators/component';
 
 const SEARCH_DEBOUNCE_TIME = 200;
 
 const LOAD_CARDINALITY = config.navi.searchThresholds.contains;
 
-export default Component.extend({
-  layout,
-
-  tagName: '',
-
+@templateLayout(layout)
+@tagName('')
+class DimensionSelectComponent extends Component {
   /**
    * @private
    * @property {Ember.Service} _dimensionService
    */
-  _dimensionService: service('bard-dimensions'),
+  @service('bard-dimensions') _dimensionService;
 
   /**
-   * @property _metadataService
+   * @private
+   * @property {Ember.Service} _metadataService
    */
-  _metadataService: service('bard-metadata'),
+  @service('bard-metadata') _metadataService;
 
   /**
    * @property {String} dimensionName - name of dimension to be filtered
    */
-  dimensionName: readOnly('filter.subject.name'),
+  @readOnly('filter.subject.name') dimensionName;
 
   /**
    * @property {String} primaryKey - primary key for this dimension
    */
-  primaryKey: readOnly('filter.subject.primaryKeyFieldName'),
+  @readOnly('filter.subject.primaryKeyFieldName') primaryKey;
 
   /**
    * @property {BardDimensionArray} dimensionOptions - list of all dimension values
    */
-  dimensionOptions: computed('filter.subject', function() {
+  @computed('dimensionName', 'searchTerm')
+  get dimensionOptions() {
+    if (this.searchTerm !== undefined) {
+      return undefined; // we are searching, so only show search results
+    }
+
     const dimensionName = get(this, 'dimensionName'),
       dimensionService = get(this, '_dimensionService'),
       metadataService = get(this, '_metadataService');
@@ -62,12 +67,13 @@ export default Component.extend({
     }
 
     return undefined;
-  }),
+  }
 
   /**
    * @property {BardDimensionArray} selectedDimensions - list of currently selected dimension values
    */
-  selectedDimensions: computed('filter.values', function() {
+  @computed('filter.values')
+  get selectedDimensions() {
     let dimensionIds = get(this, 'filter.values'),
       dimensionName = get(this, 'dimensionName'),
       primaryKey = get(this, 'primaryKey'),
@@ -82,25 +88,27 @@ export default Component.extend({
     } else {
       return resolve(A());
     }
-  }),
+  }
 
   /**
    * @property {String} filterValueFieldId - which id field to use as ID display.
    */
-  filterValueFieldId: computed('dimensionName', 'filter.field', function() {
+  @computed('dimensionName', 'filter.field')
+  get filterValueFieldId() {
     const { dimensionName } = this,
       metadataService = this._metadataService,
       meta = metadataService.getById('dimension', dimensionName);
 
     return meta ? meta.idFieldName : this.filter.field;
-  }),
+  }
 
   /**
    * @property {Boolean} useNewSearchAPI - whether to use /search endpoint instead of /values
    */
-  useNewSearchAPI: computed(function() {
+  @computed
+  get useNewSearchAPI() {
     return featureFlag('newDimensionsSearchAPI');
-  }),
+  }
 
   /**
    * Executes a dimension search for a given term and executes the
@@ -120,34 +128,36 @@ export default Component.extend({
     get(this, '_dimensionService')
       .search(dimension, { term, useNewSearchAPI })
       .then(resolve, reject);
-  },
+  }
 
-  actions: {
-    /**
-     * @action setValues
-     * @param {Array} values
-     */
-    setValues(values) {
-      let primaryKey = get(this, 'primaryKey');
-      this.onUpdateFilter({
-        rawValues: A(values).mapBy(primaryKey)
+  /**
+   * @action setValues
+   * @param {Array} values
+   */
+  @action
+  setValues(values) {
+    this.onUpdateFilter({
+      rawValues: A(values).mapBy(this.primaryKey)
+    });
+  }
+
+  /**
+   * Searches dimension service for the given query
+   *
+   * @action searchDimensionValues
+   * @param {String} searchTerm - Search query
+   */
+  @action
+  searchDimensionValues(searchTerm) {
+    let term = searchTerm.trim();
+    set(this, 'searchTerm', term);
+
+    if (term) {
+      return new Promise((resolve, reject) => {
+        return debounce(this, this._performSearch, term, resolve, reject, SEARCH_DEBOUNCE_TIME);
       });
-    },
-
-    /**
-     * Searches dimension service for the given query
-     *
-     * @action searchDimensionValues
-     * @param {String} query - Search query
-     */
-    searchDimensionValues(query) {
-      let term = query.trim();
-
-      if (term) {
-        return new Promise((resolve, reject) => {
-          return debounce(this, this._performSearch, term, resolve, reject, SEARCH_DEBOUNCE_TIME);
-        });
-      }
     }
   }
-});
+}
+
+export default DimensionSelectComponent;
