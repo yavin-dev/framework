e589acf1de0ad0b328d9f66846a90d66b477466a
George Lydakis <george.lydakis@oath.com>
2020-01-14 11:23:06 -0600

    New: Rename component (refs #624)

--- packages/search/tests/integration/components/navi-search-result-test.js
+++ packages/search/tests/integration/components/navi-search-result-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | navi-search-results', function(hooks) {
+module('Integration | Component | navi-search-result', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<NaviSearchResults />`);
+    await render(hbs`<NaviSearchResult />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <NaviSearchResults>
+      <NaviSearchResult>
         template block text
-      </NaviSearchResults>
+      </NaviSearchResult>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
