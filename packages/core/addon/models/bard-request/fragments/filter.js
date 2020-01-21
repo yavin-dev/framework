3e416860d8301236e21808408fdbdcdf7d8d07db
Kevin Hinterlong <hinterlong@verizonmedia.com>
2020-01-21 13:56:38 -0600

    Update: Revert debugger

--- packages/core/addon/models/bard-request/fragments/filter.js
+++ packages/core/addon/models/bard-request/fragments/filter.js
@@ -58,10 +58,7 @@ export default Fragment.extend(Validations, {
           dimensionService = get(this, 'dimensionService');
 
         return DS.PromiseArray.create({
-          promise: dimensionService.find(dimensionName, { values }).then(values => {
-            debugger;
-            return arr(values);
-          })
+          promise: dimensionService.find(dimensionName, { values }).then(values => arr(values))
         });
       }
     },
