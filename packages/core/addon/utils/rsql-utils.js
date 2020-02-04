90ce3850128f69137dd46e8220d7ff721b957344
George Lydakis <george.lydakis@oath.com>
2020-02-04 16:33:22 -0600

    New: Linting

--- packages/core/addon/utils/rsql-utils.js
+++ packages/core/addon/utils/rsql-utils.js
@@ -13,12 +13,12 @@
  * to a list of all the OR parameters, ie., [H, Revenue]
  */
 export function getFilterParams(queryFilter) {
-  if (queryFilter != null || queryFilter != '') {
+  if (queryFilter != null && queryFilter != '') {
     if (queryFilter.includes('author')) {
       queryFilter = queryFilter.split(';')[0];
     }
     return queryFilter
-      .replace(/[()\*]/g, '')
+      .replace(/[()*]/g, '')
       .split(',')
       .map(el => el.split('=='));
   }
@@ -33,7 +33,7 @@ export function getFilterParams(queryFilter) {
  * to get the author, ie., ramvish
  */
 export function getQueryAuthor(queryFilter) {
-  if ((queryFilter != null || queryFilter != '') && queryFilter.includes('author')) {
+  if (queryFilter != null && queryFilter != '' && queryFilter.includes('author')) {
     if (queryFilter.includes('(')) {
       queryFilter = queryFilter.split(';')[1];
     }
