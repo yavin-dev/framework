438ceb0fe2a49a9aaaa2eb7afa69a41a8ea877e2
George Lydakis <george.lydakis@oath.com>
2020-02-24 15:59:10 -0600

    Update: Remove unnecessary inputs

--- packages/search/tests/unit/services/navi-search-provider-test.js
+++ packages/search/tests/unit/services/navi-search-provider-test.js
@@ -1,24 +1,11 @@
-import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Unit | Service | navi-search-provider', function(hooks) {
   setupTest(hooks);
-  setupMirage(hooks);
 
   hooks.beforeEach(async function() {
-    // Load metadata needed for request fragment
-    await this.owner.lookup('service:bard-metadata').loadMetadata();
     this.service = this.owner.lookup('service:navi-search-provider');
-    const store = this.owner.lookup('service:store'),
-      mockAuthor = store.createRecord('user', { id: 'ciela' });
-    this.owner.register(
-      'service:user',
-      Service.extend({
-        getUser: () => mockAuthor
-      })
-    );
   });
 
   test('it exists', function(assert) {
