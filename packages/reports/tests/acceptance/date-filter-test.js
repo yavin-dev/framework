413181e5a25d27c2a4801d0b0f6d93ea58affb4d
George Lydakis <george.lydakis@oath.com>
2020-03-03 15:58:20 -0600

    Update: Updating tests

--- packages/reports/tests/acceptance/date-filter-test.js
+++ packages/reports/tests/acceptance/date-filter-test.js
@@ -73,30 +73,47 @@ module('Acceptance | date filter', function(hooks) {
     assert.ok(!!$('.grouped-list__item:contains(Day)').length, 'Day is the default selected time grain');
     assert.ok(
       !!$('.filter-builder__operator:contains(Between)').length,
-      'Between is the selected filter builder operator'
+      'Between is the default selected filter builder operator'
     );
 
     const timeGrains = ['Hour', 'Day', 'Week', 'Month', 'Quarter', 'Year'];
+    const grain = 'Year';
 
-    timeGrains.forEach(async function(grain) {
+    for (const grain of timeGrains) {
       await click($(`.grouped-list__item:contains(${grain})`)[0]);
       assert.ok(
         !!$('.filter-builder__operator:contains(Between)').length,
-        'Between is the default selected filter builder operator'
+        'Between is the selected filter builder operator'
       );
 
-      // await click($('.filter-values--date-range-input__trigger')[0]);
-      // assert.ok(!!$('ember-basic-dropdown-content').length, 'Date picker window appears');
+      //Set low value
+      await clickTrigger('.filter-values--date-range-input__low-value .ember-basic-dropdown-trigger');
+      await click($('button.ember-power-calendar-day--current-month:contains(4)')[0]);
 
-      // await click($('.filter-builder__operator:contains(Current)')[0]);
-      // assert.ok(!!$('.filter-builder__operator:contains(Current)').length, 'Between is the default selected filter builder operator');
+      //Set high value
+      await clickTrigger('.filter-values--date-range-input__high-value .ember-basic-dropdown-trigger');
+      await click($('button.ember-power-calendar-day--current-month:contains(5)')[0]);
 
-      await clickTrigger($('.filter-builder__operator-dropdown')[0]);
-      await click($('li.ember-power-select-option:contains(Since)')[0]);
+      assert
+        .dom('.filter-values--date-range-input__low-value .dropdown-date-picker__trigger')
+        .includesText('04,', 'The low value is set');
+      assert
+        .dom('.filter-values--date-range-input__high-value .dropdown-date-picker__trigger')
+        .includesText('05,', 'The high value is set');
+
+      await click($('.filter-builder__operator:contains(Between) .filter-builder__select-trigger')[0]);
+      await click($('li.ember-power-select-option:contains(Current)')[0]);
       assert.ok(
-        !!$('.filter-builder__operator:contains(Since)').length,
-        'Since is the default selected filter builder operator'
+        !!$('.filter-builder__operator:contains(Current)').length,
+        'Current is the selected filter builder operator'
       );
-    });
+
+      await click($('.filter-builder__operator:contains(Current) .filter-builder__select-trigger')[0]);
+      await click($('li.ember-power-select-option:contains(In The Past)')[0]);
+      assert.ok(
+        !!$('.filter-builder__operator:contains(In The Past)').length,
+        'In the Past is the selected filter builder operator'
+      );
+    }
   });
 });
