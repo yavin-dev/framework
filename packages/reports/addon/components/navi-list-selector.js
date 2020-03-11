25bd57c38101269dbb98569abdd37119d1a94d31
Daniel Levinson <levinson.dl@gmail.com>
2020-03-10 17:20:17 -0700

    Fix: Revert navi-list-selector.js

--- packages/reports/addon/components/navi-list-selector.js
+++ packages/reports/addon/components/navi-list-selector.js
@@ -1,84 +1,86 @@
 /**
- * Copyright 2020, Yahoo Holdings Inc.
+ * Copyright 2017, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
- *   <NaviListSelector
- *      @title={{title}}
- *      @items={{items}}
- *      @searchField={{searchField}}
- *      @selected={{selected}}
- *      @contentClass={{contentClass}}
+ *   {{#navi-list-selector
+ *      title=title
+ *      items=items
+ *      searchField=field
+ *      selected=selected
  *      as | items |
- *   >
+ *   }}
  *      {{yield items}}
- *   </NaviListSelector>
+ *   {{/navi-list-selector}}
  */
 
 import Component from '@ember/component';
-import { set, computed } from '@ember/object';
+import { set, get, computed } from '@ember/object';
 import layout from '../templates/components/navi-list-selector';
 import { searchRecords } from 'navi-core/utils/search';
-import { layout as templateLayout, tagName } from '@ember-decorators/component';
 
-@templateLayout(layout)
-@tagName('')
-class NaviListSelector extends Component {
+export default Component.extend({
+  layout,
+
+  /*
+   * @property {Array} classNames
+   */
+  classNames: ['navi-list-selector'],
+
   /*
    * @property {Boolean} showSelected
    */
-  showSelected = false;
+  showSelected: false,
 
   /*
    * @property {String} errorMessage
    */
-  @computed('query', 'showSelected', 'title')
-  get errorMessage() {
-    if (this.query?.length > 0) {
-      return `No ${this.title?.toLowerCase()} found`;
-    } else if (this.showSelected) {
-      return `No ${this.title?.toLowerCase()} selected`;
+  errorMessage: computed('query', 'showSelected', 'title', function() {
+    if (get(this, 'query.length') > 0) {
+      return `No ${get(this, 'title').toLowerCase()} found`;
+    } else if (get(this, 'showSelected')) {
+      return `No ${get(this, 'title').toLowerCase()} selected`;
     }
-    return '';
-  }
+  }),
 
   /**
    * @property {Boolean} areItemsFiltered - true if items are filtered by selected state or a search query
    */
-  @computed('showSelected', 'query')
-  get areItemsFiltered() {
+  areItemsFiltered: computed('showSelected', 'query', function() {
     return !!this.showSelected || !!this.query;
-  }
+  }),
 
   /*
    * @property {Array} filteredItems - items filtered either by selected and by searchQuery
    */
-  @computed('items', 'query', 'searchField', 'selected', 'showSelected')
-  get filteredItems() {
-    const { query, showSelected } = this,
-      //set items to selected or all items based on showSelected
-      items = showSelected ? this.selected : this.items;
+  filteredItems: computed('items', 'query', 'searchField', 'selected', 'showSelected', function() {
+    let query = get(this, 'query'),
+      items;
+
+    //set items to selected or all items based on showSelected
+    if (get(this, 'showSelected')) {
+      items = get(this, 'selected');
+    } else {
+      items = get(this, 'items');
+    }
 
     //filter items by searchQuery
     if (query) {
-      return searchRecords(items, query, this.searchField);
+      return searchRecords(items, query, get(this, 'searchField'));
     }
 
     return items;
-  }
+  }),
 
   /**
    * Called when the attributes passed into the component have been changed
-   * @method didUpdateAttrs
+   *
+   * @event didUpdateAttrs
    */
   didUpdateAttrs() {
-    this._super(...arguments);
-
     // For convenience, automatically take user to "Show All" when nothing is selected
-    if (!this.selected?.length) {
+    if (!get(this, 'selected.length')) {
       set(this, 'showSelected', false);
     }
   }
-}
-
-export default NaviListSelector;
+});
