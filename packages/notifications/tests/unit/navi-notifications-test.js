d7bb1deac7816493b424a536fc0f212f985676e8
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2020-03-17 14:09:57 -0500

    New: Use number directly as timeout for notifications (#719)
    * New: Use number directly as timeout for notifications
    
    * Update: PR Comments
    
    Co-authored-by: Kevin Hinterlong <hinterlong@verizonmedia.com>

---
+++ packages/notifications/tests/unit/navi-notifications-test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | navi-notifications', function(hooks) {
+  setupTest(hooks);
+  let ServiceFactory;
+
+  hooks.beforeEach(function() {
+    ServiceFactory = this.owner.factoryFor('service:navi-notifications');
+  });
+
+  test('add notification options', function(assert) {
+    assert.expect(2);
+    const service = ServiceFactory.create();
+
+    service.notificationService = {
+      queue: [],
+      add() {}
+    };
+
+    const assertThat = (input, output, message) => {
+      service.notificationService.add = options => assert.deepEqual(options, output, message);
+      service.add(input);
+    };
+
+    assertThat(
+      { message: 'e', timeout: 'short' },
+      { message: 'e', timeout: 3000 },
+      'The timeout alias is mapped to the duration'
+    );
+    assertThat({ message: 'e', timeout: 23 }, { message: 'e', timeout: 23 }, 'A number as a timeout is not modified');
+  });
+});
