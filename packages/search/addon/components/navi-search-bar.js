2b2f15da5d4fa64ca500e8ca27c4ac207f99d4e7
George Lydakis <george.lydakis@oath.com>
2020-03-17 20:38:29 -0500

    Update: Cleaned up search bar

--- packages/search/addon/components/navi-search-bar.js
+++ packages/search/addon/components/navi-search-bar.js
@@ -1,3 +1,16 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class NaviSearchBarComponent extends Component {}
+export default class NaviSearchBarComponent extends Component {
+  @service('navi-search-provider') searchProviderService;
+
+  @tracked searchQuery;
+  @tracked searchResults;
+
+  @action
+  async search() {
+    this.searchResults = await this.searchProviderService.search(this.searchQuery);
+  }
+}
--- packages/search/tests/integration/components/navi-search-bar-test.js
+++ packages/search/tests/integration/components/navi-search-bar-test.js
@@ -1,10 +1,25 @@
+import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, click, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Integration | Component | navi-search-bar', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function() {
+    await this.owner.lookup('service:bard-metadata').loadMetadata();
+    const store = this.owner.lookup('service:store'),
+      mockAuthor = store.createRecord('user', { id: 'ciela' });
+    this.owner.register(
+      'service:user',
+      Service.extend({
+        getUser: () => mockAuthor
+      })
+    );
+  });
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
@@ -13,14 +28,28 @@ module('Integration | Component | navi-search-bar', function(hooks) {
     await render(hbs`<NaviSearchBar />`);
 
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <NaviSearchBar>
-        template block text
-      </NaviSearchBar>
-    `);
+  test('click search button no results', async function(assert) {
+    await render(hbs`<NaviSearchBar />`);
+    debugger;
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await fillIn('.search-input', 'Hello!');
+
+    await click(find('.search-button'));
+
+    assert.dom('.results').doesNotExist();
+  });
+
+  test('press enter on search button with results', async function(assert) {
+    await render(hbs`<NaviSearchBar />`);
+
+    await fillIn('.search-input', 'Revenue');
+
+    await triggerKeyEvent('.search-input', 'keydown', 'Enter');
+    debugger;
+
+    assert.dom('.results').exists();
+    assert.dom('.results').hasClass('search-result');
   });
 });
