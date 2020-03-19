7a322d1476d80ce44b7c9c26f350b7c3964763ca
George Lydakis <lydakis.george@gmail.com>
2020-03-18 21:30:45 -0500

    Update: Cleanup

--- packages/search/addon/components/navi-search-bar.js
+++ packages/search/addon/components/navi-search-bar.js
@@ -45,17 +45,4 @@ export default class NaviSearchBarComponent extends Component {
       this.searchResults = this.emptyResult;
     }
   }
-
-  validKeyCode(keycode) {
-    var valid =
-      (keycode > 47 && keycode < 58) || // number keys
-      keycode == 32 ||
-      keycode == 13 || // spacebar & return key(s) (if you want to allow carriage returns)
-      (keycode > 64 && keycode < 91) || // letter keys
-      (keycode > 95 && keycode < 112) || // numpad keys
-      (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-      (keycode > 218 && keycode < 223); // [\]' (in order)
-
-    return valid;
-  }
 }
