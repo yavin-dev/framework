54a512f1d35004ad4009683888d70fc4b6a63146
Kevin Hinterlong <hinterlong@verizonmedia.com>
2020-04-15 14:47:18 -0500

    Update: Install typescript for core

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,22 +1,36 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
-  },
-  plugins: ['ember'],
-  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['ember', 'prettier', 'qunit', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:qunit/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
   env: {
     browser: true
   },
   rules: {
     'ember/no-jquery': 'warn',
     'ember/no-new-mixins': 'warn',
-    'ember/no-observers': 'warn'
+    'ember/no-observers': 'warn',
+
+    // cleanliness & consistency
+    'prefer-const': 'off', // const has misleading safety implications
+    'prefer-rest-params': 'off', // useful for super(...arguments) calls
+
+    // typescript
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+
+    // prettier
+    'prettier/prettier': 'error',
+
+    // better handled by prettier:
+    '@typescript-eslint/indent': 'off'
   },
   globals: {
     c3: true,
@@ -46,12 +60,15 @@ module.exports = {
       plugins: ['node'],
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
         // add your custom rules and overrides for node files here
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
       })
     },
     {
       files: ['tests/**/*.js'],
       rules: {
-        'ember/no-jquery': 'off'
+        'ember/no-jquery': 'off',
+        '@typescript-eslint/no-use-before-define': 'off'
       }
     }
   ]
