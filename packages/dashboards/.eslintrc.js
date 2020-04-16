a58bf2df63b3deda8138d3fd97f2806db40c6500
Kevin Hinterlong <hinterlong@verizonmedia.com>
2020-04-16 16:40:41 -0500

    Update: Install typescript for navi-dashboards

--- packages/dashboards/.eslintrc.js
+++ packages/dashboards/.eslintrc.js
@@ -3,23 +3,37 @@ module.exports = {
     server: true
   },
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
-  },
-  plugins: ['ember'],
-  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['ember', 'prettier', 'qunit', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:qunit/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
   env: {
     browser: true
   },
   rules: {
     'ember/no-jquery': 'warn',
     'ember/no-new-mixins': 'warn',
-    'ember/no-observers': 'warn'
+    'ember/no-observers': 'warn',
+
+    // cleanliness & consistency
+    'prefer-const': 'off', // const has misleading safety implications
+    'prefer-rest-params': 'off', // useful for super(...arguments) calls
+
+    // typescript
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+
+    // prettier
+    'prettier/prettier': 'error',
+
+    // better handled by prettier:
+    '@typescript-eslint/indent': 'off'
   },
   overrides: [
     // node files
@@ -45,6 +59,8 @@ module.exports = {
       plugins: ['node'],
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
         // add your custom rules and overrides for node files here
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
       })
     },
     {
