38cd2ebecdccb9224c492691fb9dba83c27f718a
cminick <cminick@oath.com>
2020-05-01 14:43:34 -0500

    Update: RFC updates

--- packages/core/addon/models/bard-request-v2/fragments/column.js
+++ packages/core/addon/models/bard-request-v2/fragments/column.js
@@ -3,24 +3,11 @@
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
 import attr from 'ember-data/attr';
-import { validator, buildValidations } from 'ember-cp-validations';
 import BaseFragment from './base';
-
-const Validations = buildValidations({
-  type: validator('inclusion', {
-    in: ['dimension', 'metric'],
-    allowBlank: true,
-    message() {
-      const { field } = this.model;
-      return 'The `type` field of `' + field + '` column must equal to `dimension` or `metric`';
-    }
-  })
-});
 
 /**
  * @augments {BaseFragment}
  */
-export default class Column extends BaseFragment.extend(Validations) {
-  @attr('string') type;
+export default class Column extends BaseFragment {
   @attr('string') alias;
 }
