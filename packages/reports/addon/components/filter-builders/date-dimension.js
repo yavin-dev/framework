36a66cf36e8f9d8f50a2159fc0800fcde0f636ff
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2020-05-05 13:21:34 -0500

    Update: Octane filter builders (#783)
    * Update: Octane filter builders
    
    * Update: PR Comments
    
    * Update: Fixup bad rebase
    
    * Update: Fix tests

--- packages/reports/addon/components/filter-builders/date-dimension.js
+++ packages/reports/addon/components/filter-builders/date-dimension.js
@@ -1,62 +1,64 @@
 /**
- * Copyright 2018, Yahoo Holdings Inc.
+ * Copyright 2020, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
- *   {{filter-builders/date-dimension
- *       requestFragment=request.filters.firstObject
- *       request=request
- *   }}
+ *   <FilterBuilders::DateDimension
+ *       @requestFragment={{this.request.filters.firstObject}}
+ *       @request={{this.request}}
+ *   />
  */
-import Base from './base';
-import { computed, get } from '@ember/object';
+import BaseFilterBuilderComponent from './base';
+import { computed } from '@ember/object';
 import { A as arr } from '@ember/array';
 
-export default Base.extend({
+export default class DateDimensionFilterBuilderComponent extends BaseFilterBuilderComponent {
   /**
    * @property {Object} requestFragment - interval fragment from request
    */
-  requestFragment: undefined,
+  requestFragment = undefined;
 
   /**
    * @property {Array} supportedOperators
    * @override
    */
-  supportedOperators: computed(function() {
-    return [
-      {
-        id: 'gte',
-        name: 'Since (>=)',
-        valuesComponent: 'filter-values/date'
-      },
-      {
-        id: 'lt',
-        name: 'Before (<)',
-        valuesComponent: 'filter-values/date'
-      },
-      {
-        id: 'bet',
-        name: 'Between (<=>)',
-        valuesComponent: 'filter-values/dimension-date-range'
-      }
-    ];
-  }),
+  supportedOperators = [
+    {
+      id: 'gte',
+      name: 'Since (>=)',
+      valuesComponent: 'filter-values/date'
+    },
+    {
+      id: 'lt',
+      name: 'Before (<)',
+      valuesComponent: 'filter-values/date'
+    },
+    {
+      id: 'bet',
+      name: 'Between (<=>)',
+      valuesComponent: 'filter-values/dimension-date-range'
+    }
+  ];
 
   /**
    * @property {Object} filter
    * @override
    */
-  filter: computed('requestFragment.{operator,dimension,values.[]}', function() {
-    const requestFragment = get(this, 'requestFragment'),
-      serializedFilter =
-        typeof requestFragment.serialize === 'function' ? requestFragment.serialize() : requestFragment,
-      operatorId = get(requestFragment, 'operator'),
-      operator = arr(get(this, 'supportedOperators')).findBy('id', operatorId);
+  @computed('requestFragment.{operator,dimension,values.[]}')
+  get filter() {
+    const { requestFragment } = this;
+    const serializedFilter =
+      typeof requestFragment.serialize === 'function' ? requestFragment.serialize() : requestFragment;
+
+    const { values } = serializedFilter;
+
+    const { dimension, operator: operatorId } = requestFragment;
+    const operator = arr(this.supportedOperators).findBy('id', operatorId);
 
     return {
-      subject: get(requestFragment, 'dimension'),
+      subject: dimension,
       operator,
-      values: arr(get(serializedFilter, 'values'))
+      values: arr(values)
     };
-  })
-});
+  }
+}
--- packages/reports/addon/components/filter-builders/metric.js
+++ packages/reports/addon/components/filter-builders/metric.js
@@ -1,99 +1,102 @@
 /**
- * Copyright 2019, Yahoo Holdings Inc.
+ * Copyright 2020, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
- *   {{filter-builders/metric
- *       requestFragment=request.filters.firstObject
- *   }}
+ *   <FilterBuilders::Metric
+ *       @requestFragment={{this.request.filters.firstObject}}
+ *   />
  */
 
 import { A as arr } from '@ember/array';
-import Base from './base';
-import { computed, get } from '@ember/object';
-import layout from 'navi-reports/templates/components/filter-builders/metric';
+import BaseFilterBuilderComponent from './base';
 import { getOwner } from '@ember/application';
+import { computed } from '@ember/object';
+import { layout as templateLayout, tagName } from '@ember-decorators/component';
+import layout from 'navi-reports/templates/components/filter-builders/metric';
 
-export default Base.extend({
-  layout,
-
+@templateLayout(layout)
+@tagName('')
+class MetricFilterBuilderComponent extends BaseFilterBuilderComponent {
   /**
    * @property {Object[]} supportedOperators
    */
-  supportedOperators: computed(function() {
-    return [
-      {
-        id: 'gt',
-        name: 'Greater than (>)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'gte',
-        name: 'Greater than or equals (>=)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'lt',
-        name: 'Less than (<)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'lte',
-        name: 'Less than or equals (<=)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'eq',
-        name: 'Equals (=)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'neq',
-        name: 'Not equals (!=)',
-        valuesComponent: 'filter-values/value-input'
-      },
-      {
-        id: 'bet',
-        name: 'Between (<=>)',
-        valuesComponent: 'filter-values/range-input'
-      },
-      {
-        id: 'nbet',
-        name: 'Not between (!<=>)',
-        valuesComponent: 'filter-values/range-input'
-      }
-    ];
-  }),
+  supportedOperators = [
+    {
+      id: 'gt',
+      name: 'Greater than (>)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'gte',
+      name: 'Greater than or equals (>=)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'lt',
+      name: 'Less than (<)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'lte',
+      name: 'Less than or equals (<=)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'eq',
+      name: 'Equals (=)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'neq',
+      name: 'Not equals (!=)',
+      valuesComponent: 'filter-values/value-input'
+    },
+    {
+      id: 'bet',
+      name: 'Between (<=>)',
+      valuesComponent: 'filter-values/range-input'
+    },
+    {
+      id: 'nbet',
+      name: 'Not between (!<=>)',
+      valuesComponent: 'filter-values/range-input'
+    }
+  ];
 
   /**
    * @property {Object} requestFragment - having fragment from request model
    */
-  requestFragment: undefined,
+  requestFragment = undefined;
 
   /**
    * @property {String} displayName - display name for the filter with metric and parameters
    */
-  displayName: computed('filter.subject.{metric,parameters}', function() {
-    let metric = get(this, 'filter.subject');
+  @computed('filter.subject.{metric,parameters}')
+  get displayName() {
+    const metric = this.filter.subject;
     return getOwner(this)
       .lookup('service:navi-formatter')
       .formatMetric(metric.metric, metric.parameters);
-  }),
+  }
 
   /**
    * @property {Object} filter
    * @override
    */
-  filter: computed('requestFragment.{operator,metric,values.[]}', function() {
-    const metricFragment = get(this, 'requestFragment'),
-      operatorId = get(metricFragment, 'operator'),
-      operator = arr(get(this, 'supportedOperators')).findBy('id', operatorId);
+  @computed('requestFragment.{operator,metric,values.[]}')
+  get filter() {
+    const { requestFragment } = this;
+    const { metric, values, validations, operator: operatorId } = requestFragment;
+    const operator = arr(this.supportedOperators).findBy('id', operatorId);
 
     return {
-      subject: get(metricFragment, 'metric'),
+      subject: metric,
       operator,
-      values: arr(get(metricFragment, 'values')),
-      validations: get(metricFragment, 'validations')
+      values: arr(values),
+      validations
     };
-  })
-});
+  }
+}
+
+export default MetricFilterBuilderComponent;
