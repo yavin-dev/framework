f21f957d289366f451e2724b71b34bdf791b7458
Matt Graham <mgraham@yahoo-inc.com>
2020-05-27 12:19:44 -0500

    Update: Use get-shallow when using column id as key

---
+++ packages/core/addon/helpers/get-shallow.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ *
+ * A get helper that only goes one level deep. This is necessary for keys that contain "."
+ * e.g.
+ * obj = { 'foo.bar': 'baz' };
+ * {{get-shallow obj "foo.bar"}}
+ * -> 'baz'
+ * {{get obj "foo.bar"}}
+ * -> undefined
+ */
+import { helper } from '@ember/component/helper';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type params = [Dict<any>, string];
+export function getShallow(params: params) {
+  const [obj, key] = params;
+  return obj[key];
+}
+
+export default helper(getShallow);
