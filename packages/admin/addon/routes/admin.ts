dfa0f2a698d5538a42f3adfdbe5280c6de317260
George Lydakis <lydakis.george@gmail.com>
2020-05-28 16:54:26 -0500

    New: Add navi-admin routes (#825)
    * New: Add admin and role routes
    
    * Update: Admin route and dependencies
    
    * Update router.js
    
    * Update: Add copyrights
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
    
    * Update: Added more copyrights
    
    Co-authored-by: Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>

---
+++ packages/admin/addon/routes/admin.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class AdminRoute extends Route {
+  /**
+   * @property { Service } user
+   */
+  @service user: TODO;
+
+  /**
+   * @method model
+   * @override
+   */
+  model() {
+    return this.user.getUser();
+  }
+}
