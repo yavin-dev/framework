837c59c9121d40f89e2af37944959bcbf958415a
Jon Kilroy <kilroy@verizonmedia.com>
2020-07-09 21:44:11 -0500

    Update: Add config for docker based backstop

--- packages/app/ember-backstop/backstop_data/engine_scripts/puppet/onReady.js
+++ packages/app/ember-backstop/backstop_data/engine_scripts/puppet/onReady.js
@@ -1,9 +1,9 @@
 /* eslint-env browser, node */
 
-const debug = require('debug')('BackstopJS');
+//const debug = require('debug')('BackstopJS');
 
 module.exports = async (page, scenario) => {
-  debug('SCENARIO > ' + scenario.label);
+  //debug('SCENARIO > ' + scenario.label);
   await require('./overrideCSS')(page, scenario);
   await require('./clickAndHoverHelper')(page, scenario);
 
