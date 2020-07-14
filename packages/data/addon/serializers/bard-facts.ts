c300047c47016b59b794fd747584d92f1b678667
jkusa <jkilroy@gmail.com>
2020-07-14 15:48:24 -0500

    Update: Add initial elide serializer

--- packages/data/addon/serializers/bard-facts.ts
+++ packages/data/addon/serializers/bard-facts.ts
@@ -10,6 +10,12 @@ import NaviFactSerializer, { ResponseV1 } from './fact-interface';
 import { RequestV1, RequestV2 } from 'navi-data/adapters/fact-interface';
 
 export default class BardFactsSerializer extends EmberObject implements NaviFactSerializer {
+  /**
+   * Normalizes bard/fili response into ResponseV1
+   * @param payload {AsyncQueryResponse} - payload to normalize
+   * @param request {RequestV1} - request for response payload
+   * @returns {ResponseV1}
+   */
   normalize(payload: ResponseV1, _request: RequestV1 | RequestV2): ResponseV1 | undefined {
     if (payload) {
       return {
