c8bfe96bcd922d74be1988a4a107d21581f92cb2
Matt Graham <mattgraham1995@gmail.com>
2020-07-16 11:44:00 -0500

    Update: Remove unsupported metadata fields from graphql queries (#885)
    Co-authored-by: Matt Graham <mgraham@yahoo-inc.com>

--- packages/data/addon/gql/queries/table.ts
+++ packages/data/addon/gql/queries/table.ts
@@ -3,19 +3,70 @@
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
 import gql from 'graphql-tag';
-import TableFragment from '../fragments/table';
 
 const query = gql`
   query($ids: [String!]) {
     table(ids: $ids) {
       edges {
         node {
-          ...TableFragment
+          id
+          name
+          description
+          category
+          cardinality
+          metrics {
+            edges {
+              node {
+                id
+                name
+                description
+                category
+                valueType
+                columnTags
+                columnType
+                expression
+              }
+            }
+          }
+          dimensions {
+            edges {
+              node {
+                id
+                name
+                description
+                category
+                valueType
+                columnTags
+                columnType
+                expression
+              }
+            }
+          }
+          timeDimensions {
+            edges {
+              node {
+                id
+                name
+                description
+                category
+                valueType
+                columnTags
+                columnType
+                expression
+                supportedGrains {
+                  edges {
+                    node {
+                      grain
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
   }
-  ${TableFragment}
 `;
 
 export default query;
