4260d563d2a2bf2d35ac717504f351f55597d12f
Daksh Varshney <daksh.varshney@verizonmedia.com>
2020-07-20 01:42:23 -0500

    QueryStats Route Added

--- packages/admin/tests/acceptance/admin-test.ts
+++ packages/admin/tests/acceptance/admin-test.ts
@@ -6,17 +6,20 @@ import { linkContains } from 'navi-core/test-support/contains-helpers';
 // @ts-ignore
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Acceptance | Admin', function (hooks) {
+module('Acceptance | Admin', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('navigate to roles route', async function (assert) {
-    assert.expect(2);
+  test('navigate to roles route', async function(assert) {
+    assert.expect(3);
 
     await visit('/admin');
     assert.equal(currentRouteName(), 'admin.index', 'loads admin route');
 
     await click(linkContains('Manage Users'));
     assert.equal(currentRouteName(), 'admin.users', 'loads users route');
+
+    await click(linkContains('Query Stats'));
+    assert.equal(currentRouteName(), 'admin.querystats', 'loads admin querystats');
   });
 });
