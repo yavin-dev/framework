7d960bd962f3fc818cf8c12a13160bb9a65cf8d8
Daksh Varshney <daksh.varshney@verizonmedia.com>
2020-07-24 14:44:33 -0500

    Do not Merge

--- packages/admin/addon/adapters/application.js
+++ packages/admin/addon/adapters/application.js
@@ -3,4 +3,8 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   namespace: 'admin'
   //set the host here
+  //import route from core => makes more sense
+  //something wrong with config maybe
+  //json api base class to see how it is set
+  //base-json-api-adapter
 });
