4e1c5243d46f0d26dbc526fb6ad4303412f1eab0
Matt Graham <mattgraham1995@gmail.com>
2020-07-29 11:37:40 -0500

    Update: Elide-facts adapter can send queries with RequestV2 (#905)
    * Update: Elide-facts adapter can send queries with RequestV2
    
    * Update: PR comments
    
    * Update: Test utility for escape characters
    
    Co-authored-by: Matt Graham <mgraham@yahoo-inc.com>

--- packages/data/addon/adapters/elide-facts.ts
+++ packages/data/addon/adapters/elide-facts.ts
@@ -4,7 +4,14 @@
  */
 import EmberObject from '@ember/object';
 import { queryManager } from 'ember-apollo-client';
-import NaviFactAdapter, { RequestV1, RequestOptions, AsyncQueryResponse, QueryStatus } from './fact-interface';
+import NaviFactAdapter, {
+  RequestV1,
+  RequestOptions,
+  AsyncQueryResponse,
+  QueryStatus,
+  RequestV2,
+  Parameters
+} from './fact-interface';
 import { DocumentNode } from 'graphql';
 import GQLQueries from 'navi-data/gql/fact-queries';
 import { task, timeout } from 'ember-concurrency';
@@ -18,6 +25,12 @@ interface RequestDimension {
   dimension: string;
 }
 
+const OPERATOR_MAP: Dict<string> = {
+  eq: '==',
+  neq: '!=',
+  notin: 'out'
+};
+
 export default class ElideFacts extends EmberObject implements NaviFactAdapter {
   /**
    * @property {Object} apollo - apollo client query manager using the overridden elide service
@@ -29,6 +42,19 @@ export default class ElideFacts extends EmberObject implements NaviFactAdapter {
    * @property {Number} _pollingInterval - number of ms between fetch requests during async request polling
    */
   _pollingInterval = 3000;
+
+  /**
+   * ex: { field: 'foo', parameters: { bar: 'baz', bang: 'boom' } } -> 'foo(bar: baz,bang: boom)'
+   * @param name
+   * @param parameters
+   */
+  private queryStrForField(name: string, parameters: Parameters = {}) {
+    const params = Object.keys(parameters)
+      .map(key => `${key}: ${parameters[key]}`)
+      .join(',');
+
+    return params.length ? `${name}(${params})` : name;
+  }
 
   /**
    * @param request
@@ -52,12 +78,49 @@ export default class ElideFacts extends EmberObject implements NaviFactAdapter {
 
   /**
    * @param request
+   * @returns graphql query string for a v2 request
+   */
+  private dataQueryFromRequestV2(request: RequestV2): string {
+    const args = [];
+    const { table, columns, sorts, limit, filters } = request;
+    const columnsStr = columns.map(col => this.queryStrForField(col.field, col.parameters)).join(' ');
+
+    const filterStrings = filters.map(filter => {
+      const { field, parameters, operator, values } = filter;
+      const fieldStr = this.queryStrForField(field, parameters);
+      const operatorStr = OPERATOR_MAP[operator] || `=${operator}=`;
+      const valuesStr = `(${values.join(',')})`;
+      return `${fieldStr}${operatorStr}${valuesStr}`;
+    });
+    filterStrings.length && args.push(`filter: \\"${filterStrings.join(';')}\\"`);
+
+    const sortStrings = sorts.map(sort => {
+      const { field, parameters, direction } = sort;
+      const column = this.queryStrForField(field, parameters);
+      return `${direction === 'desc' ? '-' : ''}${column}`;
+    });
+    sortStrings.length && args.push(`sort: \\"${sortStrings.join(',')}\\"`);
+
+    const limitStr = limit ? `first: \\"${limit}\\"` : null;
+    limitStr && args.push(limitStr);
+
+    const argsString = args.length ? `(${args.join(',')})` : '';
+
+    return JSON.stringify({
+      query: `{ ${table}${argsString} { edges { node { ${columnsStr} } } } }`
+    });
+  }
+
+  /**
+   * @param request
    * @param options
    * @returns Promise that resolves to the result of the AsyncQuery creation mutation
    */
-  createAsyncQuery(request: RequestV1, options: RequestOptions = {}): Promise<AsyncQueryResponse> {
+  createAsyncQuery(request: RequestV1 | RequestV2, options: RequestOptions = {}): Promise<AsyncQueryResponse> {
     const mutation: DocumentNode = GQLQueries['asyncFactsMutation'];
-    const query = this.dataQueryFromRequest(request);
+    const query = request.requestVersion?.startsWith('2.')
+      ? this.dataQueryFromRequestV2(request)
+      : this.dataQueryFromRequest(request);
     const id: string = options.requestId || v1();
 
     // TODO: Add other options based on RequestOptions
--- packages/data/tests/unit/adapters/elide-facts-test.ts
+++ packages/data/tests/unit/adapters/elide-facts-test.ts
@@ -3,6 +3,7 @@ import { setupTest } from 'ember-qunit';
 import { asyncFactsMutationStr } from 'navi-data/gql/mutations/async-facts';
 import { asyncFactsCancelMutationStr } from 'navi-data/gql/mutations/async-facts-cancel';
 import { asyncFactsQueryStr } from 'navi-data/gql/queries/async-facts';
+import { RequestV2 } from 'navi-data/adapters/fact-interface';
 import Pretender from 'pretender';
 import config from 'ember-get-config';
 
@@ -16,11 +17,39 @@ const TestRequest = {
   filters: [
     { dimension: 'd3', operator: 'in', values: ['v1', 'v2'] },
     { dimension: 'd4', operator: 'in', values: ['v3', 'v4'] },
-    { dimension: 'd5', operator: 'notnull', values: [''] }
+    { dimension: 'd5', operator: 'isnull', values: ['false'] }
   ],
   intervals: [{ start: '2015-01-03', end: '2015-01-04' }],
   having: [{ metric: 'm1', operator: 'gt', values: [0] }]
 };
+
+const TestRequestV2: RequestV2 = {
+  table: 'table1',
+  columns: [
+    { field: 'm1', type: 'metric', parameters: {} },
+    { field: 'm2', type: 'metric', parameters: {} },
+    { field: 'r', type: 'metric', parameters: { p: '123', as: 'a' } },
+    { field: 'd1', type: 'dimension', parameters: {} },
+    { field: 'd2', type: 'dimension', parameters: {} }
+  ],
+  filters: [
+    { field: 'd3', operator: 'in', values: ['v1', 'v2'], type: 'dimension', parameters: {} },
+    { field: 'd4', operator: 'in', values: ['v3', 'v4'], type: 'dimension', parameters: {} },
+    { field: 'd5', operator: 'isnull', values: ['false'], type: 'dimension', parameters: {} },
+    { field: 'time', operator: 'ge', values: ['2015-01-03'], type: 'timeDimension', parameters: {} },
+    { field: 'time', operator: 'lt', values: ['2015-01-04'], type: 'timeDimension', parameters: {} },
+    { field: 'm1', operator: 'gt', values: ['0'], type: 'metric', parameters: {} }
+  ],
+  sorts: [{ field: 'd1', parameters: {}, type: 'dimension', direction: 'asc' }],
+  limit: '10000',
+  requestVersion: '2.0',
+  dataSource: 'dummy-graphql'
+};
+
+// Double the escaped characters as well as escape the double quote character
+function escapeQuotes(str: string) {
+  return str.replace(/\\"/g, '\\\\\\"');
+}
 
 let Server: Pretender;
 
@@ -40,13 +69,115 @@ module('Unit | Adapter | elide facts', function(hooks) {
     assert.ok(adapter, 'elide-fact adapter exists');
   });
 
+  test('queryStrForField', function(assert) {
+    const adapter = this.owner.lookup('adapter:elide-facts');
+
+    assert.equal(
+      adapter.queryStrForField('foo', { bar: 'baz' }),
+      'foo(bar: baz)',
+      'Field with parameter is formatted correctly'
+    );
+    assert.equal(
+      adapter.queryStrForField('foo', { bar: 'baz', bang: 'boom' }),
+      'foo(bar: baz,bang: boom)',
+      'Field with multiple parameters is formatted correctly'
+    );
+    assert.equal(adapter.queryStrForField('foo'), 'foo', 'Name is returned for field with no parameters');
+  });
+
   test('dataQueryFromRequest', function(assert) {
     const adapter = this.owner.lookup('adapter:elide-facts');
     const queryStr = adapter.dataQueryFromRequest(TestRequest);
-    assert.deepEqual(
+    assert.equal(
       queryStr,
       '{"query":"{ table1 { edges { node { m1 m2 r d1 d2 } } } }"}',
       'dataQueryFromRequest returns the correct query string for the given request'
+    );
+  });
+
+  test('dataQueryFromRequestV2', function(assert) {
+    const adapter = this.owner.lookup('adapter:elide-facts');
+    const queryStr = adapter.dataQueryFromRequestV2(TestRequestV2);
+    assert.equal(
+      queryStr,
+      escapeQuotes(
+        '{"query":"{ table1(filter: \\"d3=in=(v1,v2);d4=in=(v3,v4);d5=isnull=(false);time=ge=(2015-01-03);time=lt=(2015-01-04);m1=gt=(0)\\",sort: \\"d1\\",first: \\"10000\\") { edges { node { m1 m2 r(p: 123,as: a) d1 d2 } } } }"}'
+      ),
+      'dataQueryFromRequestV2 returns the correct query string for the given request V2'
+    );
+
+    assert.equal(
+      adapter.dataQueryFromRequestV2({
+        table: 'myTable',
+        columns: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric' },
+          { field: 'd1', parameters: {}, type: 'dimension' }
+        ],
+        sorts: [],
+        filters: [],
+        requestVersion: '2.0',
+        dataSource: 'dummy-graphql'
+      }),
+      `{"query":"{ myTable { edges { node { m1(p: q) d1 } } } }"}`,
+      'Arguments are properly excluded if they are not in the request'
+    );
+
+    assert.equal(
+      adapter.dataQueryFromRequestV2({
+        table: 'myTable',
+        columns: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric' },
+          { field: 'd1', parameters: {}, type: 'dimension' }
+        ],
+        sorts: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric', direction: 'desc' },
+          { field: 'd1', parameters: {}, type: 'dimension', direction: 'asc' }
+        ],
+        filters: [],
+        requestVersion: '2.0',
+        dataSource: 'dummy-graphql'
+      }),
+      escapeQuotes(`{"query":"{ myTable(sort: \\"-m1(p: q),d1\\") { edges { node { m1(p: q) d1 } } } }"}`),
+      'Request with sorts and parameters is queried correctly'
+    );
+
+    assert.equal(
+      adapter.dataQueryFromRequestV2({
+        table: 'myTable',
+        columns: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric' },
+          { field: 'd1', parameters: {}, type: 'dimension' }
+        ],
+        sorts: [],
+        filters: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric', operator: 'in', values: ['v1', 'v2'] },
+          { field: 'd1', parameters: {}, type: 'dimension', operator: 'neq', values: ['a'] },
+          { field: 'd2', parameters: {}, type: 'dimension', operator: 'eq', values: ['b'] }
+        ],
+        requestVersion: '2.0',
+        dataSource: 'dummy-graphql'
+      }),
+      escapeQuotes(
+        `{"query":"{ myTable(filter: \\"m1(p: q)=in=(v1,v2);d1!=(a);d2==(b)\\") { edges { node { m1(p: q) d1 } } } }"}`
+      ),
+      'Request with filters and parameters is queried correctly'
+    );
+
+    assert.equal(
+      adapter.dataQueryFromRequestV2({
+        table: 'myTable',
+        columns: [
+          { field: 'm1', parameters: { p: 'q' }, type: 'metric' },
+          { field: 'd1', parameters: {}, type: 'dimension' }
+        ],
+        sorts: [],
+        filters: [],
+        limit: '5',
+        requestVersion: '2.0',
+        dataSource: 'dummy-graphql'
+      }),
+      escapeQuotes(`{"query":"{ myTable(first: \\"5\\") { edges { node { m1(p: q) d1 } } } }"}`),
+      'Request with limit is queried correctly'
     );
   });
 
@@ -106,6 +237,65 @@ module('Unit | Adapter | elide facts', function(hooks) {
     });
 
     const asyncQuery = await adapter.createAsyncQuery(TestRequest);
+
+    assert.deepEqual(asyncQuery, response, 'createAsyncQuery returns the correct response payload');
+  });
+
+  test('createAsyncQuery (RequestV2) - success', async function(assert) {
+    assert.expect(5);
+    const adapter = this.owner.lookup('adapter:elide-facts');
+
+    let response;
+    Server.post(`${HOST}/graphql`, function({ requestBody }) {
+      const requestObj = JSON.parse(requestBody);
+
+      assert.deepEqual(
+        Object.keys(requestObj.variables),
+        ['id', 'query'],
+        'createAsyncQuery sends id and query request variables'
+      );
+
+      assert.ok(uuidRegex.exec(requestObj.variables.id), 'A uuid is generated for the request id');
+
+      const expectedTable = TestRequestV2.table;
+      const expectedColumns = TestRequestV2.columns.map(c => adapter.queryStrForField(c.field, c.parameters)).join(' ');
+      const expectedArgs =
+        '(filter: \\"d3=in=(v1,v2);d4=in=(v3,v4);d5=isnull=(false);time=ge=(2015-01-03);time=lt=(2015-01-04);m1=gt=(0)\\",sort: \\"d1\\",first: \\"10000\\")';
+
+      assert.equal(
+        requestObj.variables.query.replace(/[ \t\r\n]+/g, ' '),
+        JSON.stringify({
+          query: `{ ${expectedTable}${expectedArgs} { edges { node { ${expectedColumns} } } } }`
+        }).replace(/[ \t\r\n]+/g, ' '),
+        'createAsyncQuery sends the correct query variable string'
+      );
+
+      assert.equal(
+        requestObj.query.replace(/__typename/g, '').replace(/[ \t\r\n]+/g, ''),
+        asyncFactsMutationStr.replace(/[ \t\r\n]+/g, ''),
+        'createAsyncQuery sends the correct mutation to create a new asyncQuery'
+      );
+
+      response = {
+        asyncQuery: {
+          edges: [
+            {
+              node: {
+                id: requestObj.variables.id,
+                query: requestObj.variables.query,
+                queryType: 'GRAPHQL_V1_0',
+                status: 'QUEUED',
+                result: null
+              }
+            }
+          ]
+        }
+      };
+
+      return [200, { 'Content-Type': 'application/json' }, JSON.stringify({ data: response })];
+    });
+
+    const asyncQuery = await adapter.createAsyncQuery(TestRequestV2);
 
     assert.deepEqual(asyncQuery, response, 'createAsyncQuery returns the correct response payload');
   });
