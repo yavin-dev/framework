765e25e0cd0d39125a46ed573a4fe19f567b0013
Daksh Varshney <daksh.varshney@verizonmedia.com>
2020-07-29 10:45:23 -0500

    Update: Template Added and Erros Fixed

--- packages/admin/addon/controllers/admin/querystats.ts
+++ packages/admin/addon/controllers/admin/querystats.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
 import Controller from '@ember/controller';
 
 export default class AdminQuerystats extends Controller.extend({
