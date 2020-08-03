ef57336e4d5159556d615c3acb5afb0b5357706b
Daksh Varshney <daksh.varshney@verizonmedia.com>
2020-08-03 15:30:03 -0500

    Update: Carbon QueryLogger Added

---
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/beans/utilities/QueryStatus.kt
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+package com.yahoo.navi.ws.models.beans.utilities
+
+enum class QueryStatus {
+    ACCEPTED,
+    COMPLETED,
+    CANCELLED,
+    FAILED,
+    INPROGRESS
+}
