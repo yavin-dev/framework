b413bd84a1d9f28af113b181fdc63e3e7e74d583
jkusa <jkilroy@gmail.com>
2020-08-07 15:07:14 -0500

    Update: Use navi metadata service in directory

--- packages/directory/tests/dummy/app/routes/application.ts
+++ packages/directory/tests/dummy/app/routes/application.ts
@@ -1,19 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import UserService from 'navi-core/services/user';
+import NaviMetadataService from 'navi-data/services/navi-metadata';
 
 export default class Application extends Route {
-  /**
-   * @property {Ember.Service}
-   */
-  @service user: TODO;
+  @service
+  private user!: UserService;
 
-  /**
-   * @property {Ember.Service}
-   */
-  @service bardMetadata: TODO;
+  @service
+  private naviMetadata!: NaviMetadataService;
 
   async model() {
-    await Promise.all([this.user.findOrRegister(), this.bardMetadata.loadMetadata()]);
+    await Promise.all([this.user.findOrRegister(), this.naviMetadata.loadMetadata()]);
   }
 
   /**
