58798a512bbec533edc7571054ec3329ffa11659
Jon Kilroy <jkilroy@gmail.com>
2020-08-14 15:09:06 -0500

    Update: Fix elide request and response names (#962)

--- packages/data/addon/serializers/facts/elide.ts
+++ packages/data/addon/serializers/facts/elide.ts
@@ -8,19 +8,44 @@
 import EmberObject from '@ember/object';
 import NaviFactSerializer, { ResponseV1 } from './interface';
 import { AsyncQueryResponse, RequestV2 } from 'navi-data/adapters/facts/interface';
+import { getElideField } from 'navi-data/adapters/facts/elide';
+import { canonicalizeMetric } from 'navi-data/utils/metric';
 
 export default class ElideFactsSerializer extends EmberObject implements NaviFactSerializer {
-  normalize(payload: AsyncQueryResponse, request: RequestV2): ResponseV1 | undefined {
-    const requestTable = request.table;
-    const responseStr = payload?.asyncQuery.edges[0].node.result?.responseBody;
-    if (responseStr) {
-      const response = JSON.parse(responseStr);
-      const rawRows = response.data[requestTable].edges;
-      return {
-        rows: rawRows.map((row: { node: TODO }) => row.node),
-        meta: {}
-      };
+  /**
+   * @param payload - raw payload string
+   * @param request - request object
+   */
+  private processResponse(payload: string, request: RequestV2): ResponseV1 {
+    const response = JSON.parse(payload);
+
+    const { table } = request;
+    // TODO revisit when Elide adds parameter support
+    const elideFields = request.columns.map(({ field, parameters }) => getElideField(field, parameters));
+    const normalizedFields = request.columns.map(({ field: metric, parameters }) =>
+      // TODO rename with generic canonicalizeColumn
+      canonicalizeMetric({ metric, parameters })
+    );
+
+    const rawRows = response.data[table].edges;
+    const totalRows = rawRows.length;
+    const totalFields = normalizedFields.length;
+    const rows = new Array(totalRows);
+
+    let r = totalRows;
+    while (r--) {
+      const rawRow = rawRows[r].node;
+      const newRow: Record<string, unknown> = (rows[r] = {});
+      let f = totalFields;
+      while (f--) {
+        newRow[normalizedFields[f]] = rawRow[elideFields[f]];
+      }
     }
-    return undefined;
+    return { rows, meta: {} };
+  }
+
+  normalize(payload: AsyncQueryResponse, request: RequestV2): ResponseV1 | undefined {
+    const responseStr = payload?.asyncQuery.edges[0].node.result?.responseBody;
+    return responseStr ? this.processResponse(responseStr, request) : undefined;
   }
 }
