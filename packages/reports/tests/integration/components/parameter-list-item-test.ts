6ada1fddcdb5614319648700c0448e948942f73e
cminick <cminick@oath.com>
2020-08-17 16:17:36 -0500

    Fix: Use dom assertions

--- packages/reports/tests/integration/components/parameter-list-item-test.ts
+++ packages/reports/tests/integration/components/parameter-list-item-test.ts
@@ -12,17 +12,17 @@ module('Integration | Component | parameter-list-item', function(hooks) {
 
     await render(hbs`<ParameterListItem @argument={{this.arg}} @parameters={{this.allParams}} />`);
 
-    assert.equal(this.element?.textContent?.trim(), 'foo (sid)', 'should show id when name is duplicated');
+    assert.dom(this.element).hasText('foo (sid)', 'should show id when name is duplicated');
 
     this.set('allParams', [{ name: 'foo' }, { name: 'baz' }, { name: 'bar' }]);
-    assert.equal(this.element?.textContent?.trim(), 'foo', 'Shows name when there is no duplicate');
+    assert.dom(this.element).hasText('foo', 'Shows name when there is no duplicate');
 
     this.set('arg', { description: 'foo', id: 'sid' });
     this.set('allParams', [{ description: 'foo' }, { description: 'foo' }, { description: 'bar' }]);
 
-    assert.equal(this.element?.textContent?.trim(), 'foo (sid)', 'should show id when description is duplicated');
+    assert.dom(this.element).hasText('foo (sid)', 'should show id when description is duplicated');
     this.set('allParams', [{ description: 'foo' }, { description: 'baz' }, { description: 'bar' }]);
-    assert.equal(this.element?.textContent?.trim(), 'foo', 'Shows description when there is no duplicate');
+    assert.dom(this.element).hasText('foo', 'Shows description when there is no duplicate');
 
     this.set('arg', { name: 'foo', id: 'sid' });
     this.set('allParams', [
@@ -30,20 +30,12 @@ module('Integration | Component | parameter-list-item', function(hooks) {
       { groupName: 'two', options: [{ name: 'baz' }, { name: 'bam' }] },
       { name: 'foo' }
     ]);
-    assert.equal(
-      this.element?.textContent?.trim(),
-      'foo (sid)',
-      'Shows id when there is a duplicate in grouped param list'
-    );
+    assert.dom(this.element).hasText('foo (sid)', 'Shows id when there is a duplicate in grouped param list');
 
     this.set('allParams', [
       { groupName: 'one', options: [{ name: 'foo' }, { name: 'bar' }] },
       { groupName: 'two', options: [{ name: 'baz' }, { name: 'bam' }] }
     ]);
-    assert.equal(
-      this.element?.textContent?.trim(),
-      'foo',
-      'Shows name when there is no duplicate in grouped param list'
-    );
+    assert.dom(this.element).hasText('foo', 'Shows name when there is no duplicate in grouped param list');
   });
 });
