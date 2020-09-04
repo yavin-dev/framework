23e0e0409d55c159a82676dfa104debd38c86d7c
Aaron Klish <aklish@gmail.com>
2020-09-04 09:21:24 -0500

    Host navi static assets in elide (#940)
    Hosting Navi static assets from the Elide service.
    
    Co-authored-by: Aaron Klish <klish@verizonmedia.com>
    Co-authored-by: Jon Kilroy <jkilroy@gmail.com>

--- packages/webservice/app/build.gradle.kts
+++ packages/webservice/app/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.moowork.gradle.node.npm.NpmTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 description = "app"
@@ -5,6 +6,7 @@ description = "app"
 plugins {
     id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("com.moowork.node") version "1.3.1"
     kotlin("jvm")
     kotlin("plugin.spring") version "1.3.72"
 }
@@ -29,6 +31,13 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<ProcessResources> {
+    dependsOn("copyNaviApp")
 }
 
 tasks.withType<KotlinCompile> {
@@ -36,4 +45,25 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "1.8"
     }
+}
+
+tasks.register<NpmTask>("installUIDependencies") {
+    setArgs(listOf("ci"))
+    setExecOverrides(closureOf<ExecSpec> {
+        setWorkingDir("../../../")
+    })
+}
+
+tasks.register<Exec>("buildUI") {
+    dependsOn("installUIDependencies")
+
+    workingDir("../../..")
+
+    commandLine = listOf("npx", "lerna", "run", "prodbuild", "--scope", "navi-app", "--stream")
+}
+
+tasks.register<Copy>("copyNaviApp") {
+    dependsOn("buildUI")
+    from("../../app/dist")
+    into("$buildDir/resources/main/META-INF/resources")
 }
