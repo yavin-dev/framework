370fb26b2e400b2235637a96b402044cd7672000
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2020-09-15 14:34:13 -0500

    Request v2 viz selector (#1012)
    * Update: Base visualization manifest
    
    * Update: Visualization manifests and tests

---
+++ packages/core/addon/navi-visualization-manifests/base.ts
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ *
+ * Visualization Base Manifest File
+ * This file registers the visualization with navi
+ *
+ */
+import { assert } from '@ember/debug';
+import EmberObject from '@ember/object';
+import RequestFragment from 'navi-core/models/bard-request-v2/request';
+
+export default class NaviVisualizationBaseManifest extends EmberObject {
+  name!: string;
+  niceName!: string;
+  icon!: string;
+
+  /**
+   * checks if the request has single metric
+   */
+  hasSingleMetric(request: RequestFragment): boolean {
+    return request.metricColumns.length === 1;
+  }
+
+  /**
+   * checks if the request has no metrics
+   */
+  hasNoMetric(request: RequestFragment): boolean {
+    return request.metricColumns.length === 0;
+  }
+
+  /**
+   * checks if the request has some metrics
+   */
+  hasMetric(request: RequestFragment): boolean {
+    return !this.hasNoMetric(request);
+  }
+
+  /**
+   * checks if the request has single time bucket
+   */
+  hasInterval(request: RequestFragment): boolean {
+    const { timeGrain, interval } = request;
+
+    return !!(timeGrain && interval);
+  }
+
+  /**
+   * checks if the request has single time bucket
+   */
+  hasSingleTimeBucket(request: RequestFragment): boolean {
+    const { timeGrain, interval } = request;
+
+    return this.hasInterval(request) && interval?.diffForTimePeriod(timeGrain) === 1;
+  }
+
+  /**
+   * checks if the request has no group by
+   */
+  hasNoGroupBy(request: RequestFragment): boolean {
+    return request.columns.filter(({ type }) => type === 'dimension').length === 0;
+  }
+
+  /**
+   * checks if the request has multiple metrics
+   */
+  hasMultipleMetrics(request: RequestFragment): boolean {
+    return request.metricColumns.length > 1;
+  }
+
+  /**
+   * checks if the request has multiple time buckets
+   */
+  hasMultipleTimeBuckets(request: RequestFragment): boolean {
+    return this.hasInterval(request) && !this.hasSingleTimeBucket(request);
+  }
+
+  /**
+   * checks if the request has a grouping dimension
+   */
+  hasGroupBy(request: RequestFragment): boolean {
+    return !this.hasNoGroupBy(request);
+  }
+
+  /**
+   * Decides whether visualization type is valid given request
+   */
+  typeIsValid(_request: RequestFragment): boolean {
+    assert(`typeIsValid is not implemented in ${this.niceName}`);
+  }
+}
---
+++ packages/core/addon/navi-visualization-manifests/line-chart.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ *
+ * Visualization Manifest File for the Line Chart Visualization
+ * This file registers the visualization with navi
+ *
+ */
+import NaviVisualizationBaseManifest from './base';
+import RequestFragment from 'navi-core/models/bard-request-v2/request';
+
+export default class LineChartManifest extends NaviVisualizationBaseManifest {
+  name = 'line-chart';
+  niceName = 'Line Chart';
+  icon = 'line-chart';
+
+  typeIsValid(request: RequestFragment) {
+    return this.hasMetric(request) && !this.hasSingleTimeBucket(request);
+  }
+}
