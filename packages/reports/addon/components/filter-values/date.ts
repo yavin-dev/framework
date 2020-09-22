ba0d7d1d1d35e33d88697ca2133f35485db47891
jkusa <jkilroy@gmail.com>
2020-09-22 13:48:48 -0500

    Update: Refactor filter-values/date

---
+++ packages/reports/addon/components/filter-values/date.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+import { readOnly } from '@ember/object/computed';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import Moment from 'moment';
+import Args from './args-interface';
+
+export default class DateComponent extends Component<Args> {
+  @readOnly('args.filter.values.0')
+  date!: string;
+
+  @action
+  setDate(date: string) {
+    this.args.onUpdateFilter({
+      values: [Moment(date).format('YYYY-MM-DD')]
+    });
+  }
+}
