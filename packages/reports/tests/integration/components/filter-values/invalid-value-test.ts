ad50b8cd09c1015c67ebc1e1b97630a1a6fc3851
jholderbaugh <jana.holderbaugh@verizonmedia.com>
2020-10-07 10:16:22 -0500

    Update: Changes to invalid-value test

--- packages/reports/tests/integration/components/filter-values/invalid-value-test.ts
+++ packages/reports/tests/integration/components/filter-values/invalid-value-test.ts
@@ -7,23 +7,22 @@ import FilterFragment from 'navi-core/models/bard-request-v2/fragments/filter';
 import FragmentFactory from 'navi-core/services/fragment-factory';
 //@ts-ignore
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import InvalidValue from 'navi-reports/components/filter-values/invalid-value';
 
-type ComponentArgs = InvalidValue['args'];
-interface TestContext extends Context, ComponentArgs {}
+interface TestContext extends Context {
+  fragmentFactory: FragmentFactory;
+  filter: FilterFragment;
+}
 
 module('Integration | Component | filter values/invalid value', function(hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   test('it renders', async function(this: TestContext, assert) {
-    assert.expect(1);
-
     this.fragmentFactory = this.owner.lookup('service:fragment-factory') as FragmentFactory;
     this.filter = this.fragmentFactory.createFilter('dimension', 'bardOne', 'age', { field: 'id' }, 'in', []);
     this.set('filter.values', undefined);
     await render(hbs`<FilterValues::InvalidValue @filter={{this.filter}} />`);
 
-    assert.dom('.filter-values--selected-error').exists();
+    assert.dom('.filter-values--selected-error').exists('filter value error is present');
   });
 });
