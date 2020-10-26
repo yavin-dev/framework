24edb290852d71d2564fc73e9cc1336a1e11b1ac
Aaron Klish <klish@verizonmedia.com>
2020-10-26 15:01:08 -0500

    Update: Removed dead files

--- docs/build.sh
+++
@@ -1,6 +0,0 @@
-[ -f node_modules/raphael/raphael.min.js ] && cp node_modules/raphael/raphael.min.js assets/js/raphael
-[ -f node_modules/treant-js/Treant.js ] && cp node_modules/treant-js/Treant.js assets/js/treant-js
-
-[ -f node_modules/treant-js/Treant.css ] && cp node_modules/treant-js/Treant.css assets/styles/treant-js
-[ -f node_modules/sass-web-fonts/_web-fonts.scss ] && cp node_modules/sass-web-fonts/_web-fonts.scss _sass
-
