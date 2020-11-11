619a06bfa68897daf2e9083e45af83d62964f389
Anup Kumangodan <anupkp@verizonmedia.com>
2020-11-11 10:11:05 -0800

    fix lint

--- packages/webservice/app/src/test/kotlin/com/yahoo/navi/ws/test/integration/StarWarsTest.kt
+++ packages/webservice/app/src/test/kotlin/com/yahoo/navi/ws/test/integration/StarWarsTest.kt
@@ -1,22 +1,20 @@
 package com.yahoo.navi.ws.test.integration
 
-import com.yahoo.navi.ws.test.framework.IntegrationTest
 import com.jayway.restassured.RestAssured.given
+import com.yahoo.navi.ws.test.framework.IntegrationTest
 import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
-import org.hamcrest.Matchers.hasItems
 
-
-class StarwarsTest: IntegrationTest() {
+class StarWarsTest : IntegrationTest() {
     @Test
-    fun simple_starwars_table_test(){
+    fun simple_starwars_table_test() {
         given()
-                .header("User","testuser")
-                .When()
-                .get("/pilots")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("data[0].type",Matchers.equalTo("pilots"))
+            .header("User", "testuser")
+            .When()
+            .get("/pilots")
+            .then()
+            .assertThat()
+            .statusCode(200)
+            .body("data[0].type", Matchers.equalTo("pilots"))
     }
 }
