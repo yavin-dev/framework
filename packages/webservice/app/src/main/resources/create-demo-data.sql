f37432a7728b688421242378ef333e6adac89c17
jkusa <jkilroy@gmail.com>
2020-11-14 09:23:12 -0600

    Update: Add netflix titles demo data

--- packages/webservice/app/src/main/resources/create-demo-data.sql
+++ packages/webservice/app/src/main/resources/create-demo-data.sql
@@ -61,3 +61,34 @@ INSERT INTO pilot_stats VALUES ('Battle of Endor', 2, 3, 24, '1983-05-25 00:00:0
 INSERT INTO pilot_stats VALUES ('Battle of Endor', 6, 1, 24, '1983-05-25 00:00:00', true);
 INSERT INTO pilot_stats VALUES ('Battle of Endor', 7, 2, 24, '1983-05-25 00:00:00', true);
 INSERT INTO pilot_stats VALUES ('Battle of Endor', 7, 2, 24, '1983-05-25 00:00:00', true);
+
+-- NETFLIX TITLES --
+
+CREATE TABLE IF NOT EXISTS netflix_titles(
+    title_id INTEGER,
+    type VARCHAR(255),
+    title VARCHAR(255),
+    director VARCHAR(255),
+    cast_list TEXT,
+    country VARCHAR(255),
+    date_added DATETIME,
+    release_year DATETIME,
+    rating VARCHAR(255),
+    duration VARCHAR(255),
+    listed_in VARCHAR(255),
+    description VARCHAR(255)
+)
+AS SELECT
+    title_id,
+    type,
+    title,
+    director,
+    cast_list,
+    country,
+    PARSEDATETIME(date_added, 'MMMMM d, yyyy'),
+    PARSEDATETIME(release_year, 'yyyy'),
+    rating,
+    duration,
+    listed_in,
+    description
+FROM CSVREAD('classpath:netflix_titles.csv');
