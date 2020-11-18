3d06b154499b2b25afec748a31a03594647b1603
Chad <cythrawll@codeangel.org>
2020-11-18 15:11:47 -0600

    Update: Screwdriver build for alpha branch (#1126)

--- scripts/artifactory-publish.sh
+++ scripts/artifactory-publish.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 set -e # Exit with nonzero exit code if anything fails
 
-echo 'Deploying navi webservice models to artifactory'
-
 cd packages/webservice
 
-./gradlew -p models artifactoryPublish
+BRANCH=$(git rev-parse --abbrev-ref HEAD);
+
+echo "Deploying navi webservice models to artifactory for branch $BRANCH"
+
+if [ $BRANCH = 'master' ]
+then
+    echo 'Publishing beta build...'
+    ./gradlew -PpublishTag=beta -p models artifactoryPublish
+elif [ $BRANCH = '0.2.x-alpha' ]
+then
+    echo 'Publishing alpha build...'
+    ./gradlew -p models artifactoryPublish
+else
+    echo 'Not a publishable branch'
+fi
--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 set -e # Exit with nonzero exit code if anything fails
 
-echo 'Publishing dev build...'
-
 # Login to NPM
 npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
 
-# Publish dev build via lerna
-npm run-script lerna-ci-publish
+BRANCH=$(git rev-parse --abbrev-ref HEAD);
+
+echo "Deploying navi npm packages for branch $BRANCH"
+
+if [ $BRANCH = 'master' ]
+then
+    echo 'Publishing beta build...'
+    npm run-script lerna-ci-publish-beta
+elif [ $BRANCH = '0.2.x-alpha' ]
+then
+    echo 'Publishing alpha build...'
+    npm run-script lerna-ci-publish
+else
+    echo 'Not a publishable branch'
+fi
