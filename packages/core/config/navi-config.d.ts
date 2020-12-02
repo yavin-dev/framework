2c06f8c5c6917c0ca5e2ff28b99ad42459252058
jkusa <jkilroy@gmail.com>
2020-12-02 15:18:40 -0800

    Fix: Deleting persistence models
    - Remove `Content-Type` header when HTTP method has no content

--- packages/core/config/navi-config.d.ts
+++ packages/core/config/navi-config.d.ts
@@ -1,6 +1,9 @@
 declare module 'navi-config' {
   export default interface NaviConfig {
     user: string;
+    appPersistence: {
+      uri: string;
+    };
     FEATURES: Record<string, boolean | undefined>;
   }
 }
