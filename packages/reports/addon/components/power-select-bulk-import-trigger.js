ec08b2861a4e60f997291c8d0bb0fc1703ddcf81
ayeswarya <ayeswarya.anandan@verizonmedia.com>
2020-12-07 15:21:28 -0500

    remove bulk import (#1149)
    * remove bulk import with todo comments and commenting onpaste method

--- packages/reports/addon/components/power-select-bulk-import-trigger.js
+++ packages/reports/addon/components/power-select-bulk-import-trigger.js
@@ -55,10 +55,10 @@ export default Trigger.extend({
 
     /**
      * Grabs text pasted into search input and opens the bulk import modal if delimeter is present
-     *
      * @action onPaste
      * @param {ClipboardEvent} pasteEvent
      */
+    // TODO Paste has been disabled since bulk import is not working
     onPaste(pasteEvent) {
       // Get pasted data via clipboard API
       let clipboardData = pasteEvent.clipboardData || window.clipboardData,
