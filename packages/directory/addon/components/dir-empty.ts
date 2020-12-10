e4a2569933bc82cd8cb410dbef88330c139f49ab
Jon Kilroy <jkilroy@gmail.com>
2020-12-10 11:56:24 -0800

    * Update: Apply yavin branding
    - Add yavin branding
    - Refactor directory table placeholders
    - Fix: Chart tooltip class collision with denali

--- packages/directory/addon/components/dir-empty.ts
+++ packages/directory/addon/components/dir-empty.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019, Yahoo Holdings Inc.
+ * Copyright 2020, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Usage:
@@ -9,5 +9,5 @@ import Component from '@glimmer/component';
 import FileTypes from 'navi-directory/utils/enums/file-types';
 
 export default class DirEmptyComponent extends Component {
-  reportRoute = FileTypes.definitions.reports.linkRoute;
+  fileTypes = Object.entries(FileTypes.definitions);
 }
