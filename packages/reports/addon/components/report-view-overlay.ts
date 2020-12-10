053422f5bd9da60318691274bc4de216b9679075
Kevin Hinterlong <hinterlong@verizonmedia.com>
2020-12-10 15:17:08 -0600

    Update: Address PR Comments

--- packages/reports/addon/components/report-view-overlay.ts
+++ packages/reports/addon/components/report-view-overlay.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2020, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
