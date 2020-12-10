9f04ee646bc1164392f732db6de6b41548a1c617
George Lydakis <lydakis.george@gmail.com>
2020-12-10 12:53:40 -0600

    Update packages/dashboards/tests/dummy/config/environment.js
    Co-authored-by: Jon Kilroy <jkilroy@gmail.com>

--- packages/dashboards/tests/dummy/config/environment.js
+++ packages/dashboards/tests/dummy/config/environment.js
@@ -31,7 +31,7 @@ module.exports = function(environment) {
       ],
       FEATURES: {
         enableDashboardExport: true,
-        exportFileTypes: [],
+        exportFileTypes: ['csv', 'pdf', 'png'],
         enableScheduleDashboards: true,
         enableDashboardFilters: true
       }
