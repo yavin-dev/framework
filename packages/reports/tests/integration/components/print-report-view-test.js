0e416e3f2a2a1b1a7e6e4209292047560f1d26ea
jholderbaugh <jana.holderbaugh@verizonmedia.com>
2020-12-11 12:36:22 -0600

    Update: Suggested print report view changes

--- packages/reports/tests/integration/components/print-report-view-test.js
+++ packages/reports/tests/integration/components/print-report-view-test.js
@@ -1,15 +1,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import hbs from 'htmlbars-inline-precompile';
 import NaviFactResponse from 'navi-data/models/navi-fact-response';
 
 const TEMPLATE = hbs`
-<PrintReportView
-  @report={{this.report}}
-  @response={{this.response}}
-/>`;
+  <PrintReportView
+    @report={{this.report}}
+    @response={{this.response}}
+  />`;
 
 const RESPONSE = NaviFactResponse.create({
   rows: [
@@ -58,79 +58,81 @@ module('Integration | Component | print report view', function(hooks) {
 
   hooks.beforeEach(async function() {
     const metadataService = this.owner.lookup('service:navi-metadata');
+    const store = this.owner.lookup('service:store');
 
     await metadataService.loadMetadata();
     this.set('response', RESPONSE);
 
     //set report object
-    this.set('report', {
-      id: 13,
-      title: 'RequestV2 testing report',
-      createdOn: '2015-04-01 00:00:00',
-      updatedOn: '2015-04-01 00:00:00',
-      authorId: 'navi_user',
-      deliveryRuleIds: [],
-      visualization: {
-        type: 'line-chart',
-        version: 2,
-        metadata: {
-          style: {
-            area: false,
-            stacked: false
-          },
-          axis: {
-            y: {
-              series: {
-                type: 'dateTime',
-                config: {
-                  timeGrain: 'day',
-                  metricCid: 'c2'
+    this.set(
+      'report',
+      store.createRecord('report', {
+        id: 13,
+        title: 'RequestV2 testing report',
+        createdOn: '2015-04-01 00:00:00',
+        updatedOn: '2015-04-01 00:00:00',
+        authorId: 'navi_user',
+        deliveryRuleIds: [],
+        visualization: {
+          type: 'line-chart',
+          version: 2,
+          metadata: {
+            style: {
+              area: false,
+              stacked: false
+            },
+            axis: {
+              y: {
+                series: {
+                  type: 'metric',
+                  config: {}
                 }
               }
             }
           }
-        }
-      },
-      request: {
-        table: 'network',
-        dataSource: 'bardOne',
-        limit: null,
-        requestVersion: '2.0',
-        filters: [
-          {
-            type: 'timeDimension',
-            source: 'bardOne',
-            field: 'network.dateTime',
-            parameters: { grain: 'day' },
-            operator: 'bet',
-            values: ['2015-11-09', '2015-11-14']
-          }
-        ],
-        columns: [
-          {
-            cid: 'c1',
-            field: 'network.dateTime',
-            parameters: {
-              grain: 'day'
+        },
+        request: {
+          table: 'network',
+          dataSource: 'bardOne',
+          limit: null,
+          requestVersion: '2.0',
+          filters: [
+            {
+              type: 'timeDimension',
+              source: 'bardOne',
+              field: 'network.dateTime',
+              parameters: { grain: 'day' },
+              operator: 'bet',
+              values: ['11-04-2020', '11-06-2020']
+            }
+          ],
+          columns: [
+            {
+              cid: 'c1',
+              field: 'network.dateTime',
+              parameters: {
+                grain: 'day'
+              },
+              type: 'timeDimension',
+              source: 'bardOne'
             },
-            type: 'timeDimension'
-          },
-          {
-            cid: 'c2',
-            type: 'metric',
-            field: 'adClicks',
-            parameters: {}
-          }
-        ],
-        sorts: []
-      }
-    });
+            {
+              cid: 'c2',
+              type: 'metric',
+              field: 'adClicks',
+              parameters: {},
+              source: 'bardOne'
+            }
+          ],
+          sorts: []
+        }
+      })
+    );
   });
 
   test('visualization is chosen based on report', async function(assert) {
     assert.expect(3);
 
-    await settled();
     await render(TEMPLATE);
 
     assert.dom('.line-chart-widget').exists('Visualization is rendered based on the report visualization type');
@@ -152,26 +154,23 @@ module('Integration | Component | print report view', function(hooks) {
     assert.dom('.line-chart-widget').doesNotExist('Old visualization is removed');
   });
 
-  test('no data', function(assert) {
+  test('no data', async function(assert) {
     assert.expect(1);
-
-    return settled().then(async () => {
-      this.set('response', {
-        rows: [],
-        meta: {
-          pagination: {
-            currentPage: 1,
-            rowsPerPage: 10000,
-            numberOfResults: 0
-          }
+    this.set('response', {
+      rows: [],
+      meta: {
+        pagination: {
+          currentPage: 1,
+          rowsPerPage: 10000,
+          numberOfResults: 0
         }
-      });
-
-      await render(TEMPLATE);
-
-      assert
-        .dom('.print-report-view__visualization-no-results')
-        .hasText('No results available.', 'A message is displayed when the response has no data');
+      }
     });
+
+    await render(TEMPLATE);
+
+    assert
+      .dom('.print-report-view__visualization-no-results')
+      .hasText('No results available.', 'A message is displayed when the response has no data');
   });
 });
