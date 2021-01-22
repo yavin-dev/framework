9246954df350a056d44214189816251daff7abba
rishi-aga <61517466+rishi-aga@users.noreply.github.com>
2021-01-22 10:23:02 -0500

    Upgrade to Elide 5.0.0-pr30 (#1205)
    Co-authored-by: Rishi Agarwal <rishi.agarwal@verizonmedia.com>

--- packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/checks/DefaultSameUserCheck.kt
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/checks/DefaultSameUserCheck.kt
@@ -9,14 +9,17 @@ import com.yahoo.elide.core.filter.Operator
 import com.yahoo.elide.core.filter.predicates.FilterPredicate
 import com.yahoo.elide.core.security.RequestScope
 import com.yahoo.elide.core.security.checks.FilterExpressionCheck
+import com.yahoo.elide.core.type.ClassType.STRING_TYPE
+import com.yahoo.elide.core.type.Type
+import com.yahoo.elide.core.utils.TypeHelper.getClassType
 import com.yahoo.navi.ws.models.beans.User
 
 open class DefaultSameUserCheck : FilterExpressionCheck<User>() {
     companion object {
         const val IS_SAME_USER = "is this same user"
     }
-    override fun getFilterExpression(entityClass: Class<*>, requestScope: RequestScope): FilterPredicate {
-        val userPath = Path.PathElement(User::class.java, String::class.java, "id")
+    override fun getFilterExpression(entityClass: Type<*>, requestScope: RequestScope): FilterPredicate {
+        val userPath = Path.PathElement(getClassType(User::class.java), STRING_TYPE, "id")
         val path = Path(listOf(userPath))
         val value = listOf(requestScope.user.name)
         return FilterPredicate(path, Operator.IN, value)
