bd65f0e244e7f322456d2acf549299bc9dd50923
George Lydakis <lydakis.george@gmail.com>
2021-01-24 16:08:11 -0600

    Update: Move search over to Sass

--- packages/search/index.js
+++ packages/search/index.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   name: require('./package').name,
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
+  },
 
   isDevelopingAddon() {
     return process.env.DEV_NAVI;
