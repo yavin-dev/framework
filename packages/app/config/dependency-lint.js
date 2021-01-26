4debb684965e9996b9b6213b295c5a9717e8c06e
jkusa <jkilroy@gmail.com>
2021-01-26 12:57:11 -0600

    Update: Ember power calendar

--- packages/app/config/dependency-lint.js
+++ packages/app/config/dependency-lint.js
@@ -4,7 +4,6 @@ module.exports = {
   allowedVersions: {
     'ember-test-waiters': '1.2.0 || 1.1.1',
     'ember-truth-helpers': '2.1.0 || 3.0.0',
-    'ember-get-config': '0.2.4 || 0.3.0',
-    'ember-assign-helper': '0.2.0' || '0.3.0'
+    'ember-get-config': '0.2.4 || 0.3.0'
   }
 };
