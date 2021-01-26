4d62689cb8d7461942b1d3e7c0f8c0a69e21c9ee
jkusa <jkilroy@gmail.com>
2021-01-26 14:47:18 -0600

    Update: Review comments

--- packages/reports/tests/integration/components/navi-email-input-test.js
+++ packages/reports/tests/integration/components/navi-email-input-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find, click } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import $ from 'jquery';
 import { typeInInput } from '../../helpers/ember-tag-input';
