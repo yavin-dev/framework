ac615ffcac268498402ba1e0ca9cb719d0f36d96
Ayeswarya Anandan <aanandan@vpn-pool-62-105.buffalo.office.oath>
2021-01-29 17:18:50 -0500

    remove blur tests

--- packages/core/tests/integration/components/navi-visualization-config/metric-label-test.ts
+++ packages/core/tests/integration/components/navi-visualization-config/metric-label-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, blur } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { TestContext as Context } from 'ember-test-helpers';
 import StoreService from '@ember-data/store';
