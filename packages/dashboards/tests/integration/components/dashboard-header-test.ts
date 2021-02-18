de1a20a1563826df5a4dc916e2cfcfd228033c9e
Ayeswarya Anandan <aanandan@unknown.yahoo.com>
2021-02-18 12:38:14 -0500

    test lint

--- packages/dashboards/tests/integration/components/dashboard-header-test.ts
+++ packages/dashboards/tests/integration/components/dashboard-header-test.ts
@@ -6,21 +6,15 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | dashboard-header', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function() {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{dashboard-header}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#dashboard-header}}
-        template block text
-      {{/dashboard-header}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    <DashboardHeader
+      @dashboard={{this.dashboard}}
+      @deleteAction={{this.deleteAction}}
+      @addWidgetToDashboard={{this.addWidgetToDashboard}}
+    />`);
   });
 });
