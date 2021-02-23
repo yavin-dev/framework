4ec2d2362dcdc06e27cd37417275c2a4b66798bb
Ayeswarya Anandan <aanandan@unknown.yahoo.com>
2021-02-23 00:27:43 -0500

    review comments changes

--- packages/dashboards/addon/components/dashboard-header.ts
+++ packages/dashboards/addon/components/dashboard-header.ts
@@ -1,10 +1,19 @@
+/**
+ * Copyright 2021, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
 import Component from '@glimmer/component';
+import FilterFragment from 'navi-core/addon/models/bard-request-v2/fragments/filter';
 import DashboardModel from 'navi-core/addon/models/dashboard';
 
 interface Args {
   dashboard: DashboardModel;
   deleteAction: (dashboard: DashboardModel) => void;
   addWidgetToDashboard: (dashboardId: number, reportName: string) => void;
+  onUpdateFilter(filter: FilterFragment): void;
+  onRemoveFilter(filter: FilterFragment): void;
+  onAddFilter(filter: FilterFragment): void;
 }
 
 export default class DashboardHeaderComponent extends Component<Args> {}
