983494e6b97f2df7f699d32685a389aff05b453e
anupkumangodan <45950966+anupkumangodan@users.noreply.github.com>
2021-03-04 15:40:34 -0800

    reduce mem

--- packages/webservice/app/build.gradle.kts
+++ packages/webservice/app/build.gradle.kts
@@ -59,7 +59,7 @@ tasks.withType<Test> {
         events("passed", "skipped", "failed")
     }
     // set JVM arguments for the test JVM(s)
-    jvmArgs ("-Xmx4608m")
+    jvmArgs ("-Xmx2048m")
 }
 
 tasks.withType<ProcessResources> {
