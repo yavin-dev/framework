cd8cfad7fc6249b9d8f4f4675c927aecd7d7977c
jkusa <jkilroy@gmail.com>
2021-03-04 15:27:27 -0600

    Update: Updates

---
+++ packages/dashboards/addon/components/dashboard-filters.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2021, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import DashboardModel from 'navi-core/models/dashboard';
+import FilterFragment from 'navi-core/models/bard-request-v2/fragments/filter';
+import Store from '@ember-data/store';
+
+interface Args {
+  dashboard: DashboardModel;
+  onRemoveFilter(filter: FilterFragment): void;
+  onUpdateFilter(filter: FilterFragment): void;
+  onAddFilter(): void;
+}
+
+export default class DashboardFiltersComponent extends Component<Args> {
+  @service
+  store!: Store;
+
+  @tracked
+  isCollapsed = true;
+
+  @tracked
+  isAddingMode = false;
+
+  @computed('args.dashboard.filters.[]')
+  get request() {
+    return this.store.createFragment('bard-request-v2/request', {
+      table: null,
+      columns: [],
+      filters: (this.args.dashboard.filters || []).map((filter) => {
+        const newFilter = this.store.createFragment('bard-request-v2/fragments/filter', {
+          field: filter.field,
+          parameters: filter.parameters,
+          type: filter.type,
+          operator: filter.operator,
+          values: filter.values,
+          source: filter.source,
+        });
+        return newFilter;
+      }),
+      sorts: [],
+      limit: null,
+      dataSource: null,
+      requestVersion: '2.0',
+    });
+  }
+}
---
+++ packages/reports/addon/components/favorite-item.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2021, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+import Component from '@glimmer/component';
+import DeliverableItemModel from 'navi-core/models/deliverable-item';
+import UserModel from 'navi-core/models/user';
+
+interface Args {
+  user: UserModel;
+  item: DeliverableItemModel;
+  onToggle(): void;
+}
+
+export default class FavoriteItemComponent extends Component<Args> {
+  get itemType(): 'report' | 'dashboard' {
+    //@ts-ignore
+    return this.args.item.constructor.modelName;
+  }
+
+  get favoriteItems(): Array<DeliverableItemModel> {
+    const { itemType } = this;
+    if ('report' === itemType) {
+      return this.args.user.favoriteReports.toArray();
+    }
+    if ('dashboard' === itemType) {
+      return this.args.user.favoriteDashboards.toArray();
+    }
+    return [];
+  }
+
+  get isFavorite() {
+    return this.favoriteItems.includes(this.args.item);
+  }
+}
