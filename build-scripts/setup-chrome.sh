acf8f1c8a9ab471f5dff8ec598a38fce6e9509e2
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-03-19 13:07:51 -0500

    Update: Explicit check for symbolic link

--- build-scripts/setup-chrome.sh
+++ build-scripts/setup-chrome.sh
@@ -2,16 +2,7 @@
 
 CHROME_DESTINATION=/usr/bin/chrome
 
-echo "Test: start";
-test -f "$CHROME_DESTINATION"; echo $?
-test -e "$CHROME_DESTINATION"; echo $?
-test -x "$CHROME_DESTINATION"; echo $?
-test -L "$CHROME_DESTINATION"; echo $?
-
-ls -l "$CHROME_DESTINATION"
-echo "Test: end";
-
-if [ -e "$CHROME_DESTINATION" ]; then
+if [ -L "$CHROME_DESTINATION" ]; then
   echo "$CHROME_DESTINATION exists. Skipping download."
 else 
   echo "$CHROME_DESTINATION does not exist. Starting download."
@@ -19,5 +10,5 @@ else
   sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
   apt-get update -qy
   apt-get install -qy google-chrome-stable
-  ln -f -s /usr/bin/google-chrome ${CHROME_DESTINATION}
+  ln -s /usr/bin/google-chrome ${CHROME_DESTINATION}
 fi
