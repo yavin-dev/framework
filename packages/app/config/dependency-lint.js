06a80d5e996f302d69360c15b5496ec43f2dd9ca
Ayeswarya Anandan <aanandan@vpn-pool-60-94.buffalo.office.oath>
2021-03-23 13:12:54 -0400

    test fix

--- packages/app/config/dependency-lint.js
+++ packages/app/config/dependency-lint.js
@@ -1,10 +1,12 @@
 'use strict';
-
 module.exports = {
   allowedVersions: {
     'ember-test-waiters': '1.2.0 || 1.1.1',
     'ember-truth-helpers': '2.1.0 || 3.0.0',
     'ember-get-config': '0.2.4 || 0.3.0',
     'ember-destroyable-polyfill': '2.0.2 || 2.0.3',
+    'ember-concurrency': '1.3.0 || 2.0.3',
+    'ember-element-helper': '0.2.0 || 0.3.2',
+    'ember-basic-dropdown': '3.0.11 || 3.0.16',
   },
 };
