fa14305befd9f0aeb3e437837fecef1b5f4806cd
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-03-29 14:13:42 -0500

    Update: Fixup lint and address comment

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -44,8 +44,10 @@ module.exports = {
       plugins: ['node'],
       extends: ['plugin:node/recommended'],
       rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        /*
+         * this can be removed once the following is fixed
+         * https://github.com/mysticatea/eslint-plugin-node/issues/77
+         */
         'node/no-unpublished-require': 'off',
       },
     },
