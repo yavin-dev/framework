d4bcfc9dd60c8e813ecb761d8364c536af1eb855
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2021-03-29 17:43:04 -0500

    Update: App from v3.15.2...v3.25.3 (#1341)

--- packages/app/config/targets.js
+++ packages/app/config/targets.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions',
+];
 
-const isCI = !!process.env.CI;
+const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
 
 if (isCI || isProduction) {
@@ -10,5 +14,5 @@ if (isCI || isProduction) {
 }
 
 module.exports = {
-  browsers
+  browsers,
 };
