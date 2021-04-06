33e2d03bd148dc405c4b93e57dbd22145fff3d0b
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-04-06 12:59:50 -0500

    Update: Fix tests

--- packages/reports/tests/integration/components/navi-loading-message-test.js
+++ packages/reports/tests/integration/components/navi-loading-message-test.js
@@ -1,23 +1,21 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import $ from 'jquery';
 import hbs from 'htmlbars-inline-precompile';
+
+const TEMPLATE = hbs`
+<NaviLoadingMessage>
+  Loading
+</NaviLoadingMessage>
+`;
 
 module('Integration | Component | navi loading message', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    assert.dom('*').hasText('');
+    await render(TEMPLATE);
 
-    // Template block usage:
-    await render(hbs`
-          {{#navi-loading-message}}
-                Loading
-          {{/navi-loading-message}}
-      `);
-
-    assert.ok($('.navi-loader'), 'the navi-loader component is rendered');
+    assert.dom('.loader').exists('the loader component is rendered');
 
     assert.dom('.navi-loading-message').hasText('Loading', 'The text inside the block is rendered as specified');
   });
