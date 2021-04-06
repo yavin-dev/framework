8091faf213f2e00693f244db1460eddf7cfe5225
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-04-06 12:04:21 -0500

    Update: Fixup deprecation workflow

--- packages/notifications/tests/test-helper.ts
+++ packages/notifications/tests/test-helper.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
