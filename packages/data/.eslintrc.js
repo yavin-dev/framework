7f81121c8fda300dd83c84b8dff8a8f1ee9d2c22
ayeswarya <ayeswarya.anandan@verizonmedia.com>
2021-04-07 09:04:10 -0400

    Update: Data to ember 3.25  (#1359)
    Upgrade Data to ember 3.25

--- packages/data/.eslintrc.js
+++ packages/data/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   rules: {
     'ember/no-jquery': 'error',
-
+    'ember/no-classic-classes': 'warn',
     // cleanliness & consistency
     'prefer-const': 'off', // const has misleading safety implications
     'prefer-rest-params': 'off', // useful for super(...arguments) calls
@@ -28,7 +28,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
 
     // prettier
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 
     // better handled by prettier:
     '@typescript-eslint/indent': 'off',
@@ -38,6 +38,7 @@ module.exports = {
     {
       files: [
         '.eslintrc.js',
+        '.prettierrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'index.js',
