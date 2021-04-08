fd9c060689c5c38265e29f57d8baae0cefc2bce9
jholderbaugh <jana.holderbaugh@verizonmedia.com>
2021-04-08 10:18:56 -0500

    Update: Functionality working, no tests made

--- packages/reports/addon/components/dropdown-parameter-picker.ts
+++ packages/reports/addon/components/dropdown-parameter-picker.ts
@@ -3,21 +3,25 @@
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
 import Component from '@glimmer/component';
+import { action, set } from '@ember/object';
 import { capitalize } from 'lodash-es';
 import FilterFragment from 'navi-core/addon/models/bard-request-v2/fragments/filter';
 
 interface Args {
   filter: FilterFragment;
   parameterIndex: number;
-  updateParameters: () => void;
+  updateParameters: (key: string, value: string) => void;
   parameterKeys: string[];
 }
 
 export default class ParameterPickerComponent extends Component<Args> {
   selectedParameter = Object.values(this.args.filter.parameters)[this.args.parameterIndex];
+  options: { groupName: string; options: string[] | undefined }[] = [];
 
-  get parameterOptions() {
-    return this.args.filter.columnMetadata.parameters[this.args.parameterIndex].values;
+  @action
+  async fetchParameterOptions() {
+    const valuesPromise = this.args.filter.columnMetadata.parameters[this.args.parameterIndex].values;
+    set(this, 'options', [{ groupName: this.parameterName, options: (await valuesPromise)?.map((el) => el.id) }]);
   }
 
   get parameterName() {
@@ -30,5 +34,11 @@ export default class ParameterPickerComponent extends Component<Args> {
 
   get parameterValue() {
     return this.args.filter.parameters[this.args.parameterKeys[this.args.parameterIndex]];
+  }
+
+  @action
+  updateSelected(id: string) {
+    set(this, 'selectedParameter', id);
+    this.args.updateParameters(this.parameterKey, this.selectedParameter);
   }
 }
