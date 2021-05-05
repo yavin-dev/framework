5d207bfd34927fc4e32286d31ec1b4a42d9e799f
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2021-05-05 15:59:50 -0500

    Update: Add displayName for server-generated-config (#1379)

--- assets/server-generated-config.js
+++ assets/server-generated-config.js
@@ -1,11 +1,11 @@
 window.NAVI_APP = {
 	version: '1.0.0',
-  dataSources: [{ name: "default", uri: "https://www.naviapp.io/fact", type: "bard" }],
-  appPersistence: {
-    type: "elide",
-    uri: "https://www.naviapp.io/persistence",
-    timeout: 90000
-  },
-  user: 'navi_user',
-	FEATURES: {}
+	dataSources: [{ name: 'default', displayName: 'Default', uri: 'https://www.naviapp.io/fact', type: 'bard' }],
+	appPersistence: {
+		type: 'elide',
+		uri: 'https://www.naviapp.io/persistence',
+		timeout: 90000,
+	},
+	user: 'navi_user',
+	FEATURES: {},
 };
