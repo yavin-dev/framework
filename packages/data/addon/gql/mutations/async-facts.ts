ac8083f8d82c984a4db32eb24e45aeb9021dfaf2
Chris Williamson <christopher.e.williamson@gmail.com>
2021-06-25 09:02:05 -0500

    Upgrade: Upgraded graphql package and adjust gql

--- packages/data/addon/gql/mutations/async-facts.ts
+++ packages/data/addon/gql/mutations/async-facts.ts
@@ -5,23 +5,26 @@
 import gql from 'graphql-tag';
 
 export const asyncFactsMutationStr = `
-  mutation($id: ID, $query: String, $asyncAfterSeconds: Int) {
-    asyncQuery(op: UPSERT, data: { id: $id, query: $query, queryType: GRAPHQL_V1_0, status: QUEUED, asyncAfterSeconds: $asyncAfterSeconds }) {
-      edges {
-        node {
-          id
-          query
-          queryType
-          status
-          result {
-            contentLength
-            responseBody
-            httpStatus
-          }
+mutation ($id: ID, $query: String, $asyncAfterSeconds: Int) {
+  asyncQuery(
+    op: UPSERT
+    data: {id: $id, query: $query, queryType: GRAPHQL_V1_0, status: QUEUED, asyncAfterSeconds: $asyncAfterSeconds}
+  ) {
+    edges {
+      node {
+        id
+        query
+        queryType
+        status
+        result {
+          contentLength
+          responseBody
+          httpStatus
         }
       }
     }
   }
+}
 `;
 
 const mutation = gql`
