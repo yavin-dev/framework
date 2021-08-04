aebf77433f79c9136579fa0f951f303b3756107c
Aaron Klish <aklish@gmail.com>
2021-08-04 13:50:01 -0500

    Update: Remove request V1 from data model
    - Upgraded to elide 5.0.6

--- packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/beans/fragments/Request.kt
+++ packages/webservice/models/src/main/kotlin/com/yahoo/navi/ws/models/beans/fragments/Request.kt
@@ -4,12 +4,21 @@
  */
 package com.yahoo.navi.ws.models.beans.fragments
 
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.yahoo.navi.ws.models.beans.fragments.request.Column
+import com.yahoo.navi.ws.models.beans.fragments.request.Filter
+import com.yahoo.navi.ws.models.beans.fragments.request.Sort
+import com.yahoo.navi.ws.models.types.JsonType
+import org.hibernate.annotations.TypeDef
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "requestVersion")
-@JsonSubTypes(
-    JsonSubTypes.Type(value = RequestV1::class, name = "v1"),
-    JsonSubTypes.Type(value = RequestV2::class, name = "2.0")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@TypeDef(typeClass = JsonType::class, name = "json")
+data class Request(
+    var filters: List<Filter> = emptyList(),
+    var columns: List<Column> = emptyList(),
+    var table: String = "",
+    var sorts: List<Sort> = emptyList(),
+    var limit: Int? = null,
+    var dataSource: String? = null,
+    val requestVersion: String = "2.0"
 )
-interface Request
