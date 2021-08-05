e84fc3e7a1a27cabcfbcec777f5437cedd766fd2
Miranda Grisa <miranda.grisa@verizonmedia.com>
2021-08-05 09:34:12 -0500

    Update: Made controller typescript safe

--- packages/core/tests/dummy/app/controllers/date-pick.ts
+++ packages/core/tests/dummy/app/controllers/date-pick.ts
@@ -1,13 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import Moment from 'moment';
+import moment, { Moment } from 'moment';
 
 export default class DatePickController extends Controller {
-  @tracked savedDate = Moment.utc('2021-08-03');
+  @tracked savedDate = moment.utc('2021-08-03');
 
   @action
-  onUpdate(date) {
-    this.savedDate = Moment.utc(date);
+  onUpdate(date: Moment) {
+    this.savedDate = moment.utc(date);
   }
 }
