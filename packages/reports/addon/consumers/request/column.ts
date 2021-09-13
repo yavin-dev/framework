7974153bc0f87901e5bc537ee2d6c2c7b5feb92e
Chad Minick <cminick@oath.com>
2021-09-13 13:42:54 -0500

    Fix: PR comments

--- packages/reports/addon/consumers/request/column.ts
+++ packages/reports/addon/consumers/request/column.ts
@@ -59,7 +59,6 @@ export default class ColumnConsumer extends ActionConsumer {
       const { routeName } = route;
       const { request } = route.modelFor(routeName) as ReportModel;
       request.removeColumn(column);
-      this.requestActionDispatcher.dispatch(RequestActions.REMOVE_ROLLUP_COLUMN, route, column);
     },
 
     /**
