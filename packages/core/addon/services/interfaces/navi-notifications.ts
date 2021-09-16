b611414b5b0510af999ca10c9bdd829b8ca28b4f
Miranda G <55207313+100starnight@users.noreply.github.com>
2021-09-16 10:55:04 -0500

    Update packages/core/addon/services/interfaces/navi-notifications.ts
    Co-authored-by: Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>

--- packages/core/addon/services/interfaces/navi-notifications.ts
+++ packages/core/addon/services/interfaces/navi-notifications.ts
@@ -2,8 +2,10 @@
  * Copyright 2021, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
-import { SafeString } from 'handlebars';
+import { htmlSafe } from '@ember/template';
 
+// TODO: Replace once resolved https://github.com/typed-ember/ember-cli-typescript/issues/1155#issue-629409268
+export type SafeString = ReturnType<typeof htmlSafe>
 export type MessageStyle = 'default' | 'info' | 'warning' | 'success' | 'danger';
 export type MessageTimeout = 'short' | 'medium' | 'long' | 'none';
 
