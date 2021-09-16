7e10b3df7b384fa523c0e02d6b694181992ab509
Miranda Grisa <miranda.grisa@verizonmedia.com>
2021-09-16 09:44:09 -0500

    Update: Add htmlSafe() type SafeString to messageOptions title

--- packages/core/addon/services/interfaces/navi-notifications.ts
+++ packages/core/addon/services/interfaces/navi-notifications.ts
@@ -2,11 +2,13 @@
  * Copyright 2021, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
+import { SafeString } from 'handlebars';
+
 export type MessageStyle = 'default' | 'info' | 'warning' | 'success' | 'danger';
 export type MessageTimeout = 'short' | 'medium' | 'long' | 'none';
 
 export interface MessageOptions {
-  title: string;
+  title: string | SafeString;
   context?: string;
   style?: MessageStyle;
   timeout?: MessageTimeout;
