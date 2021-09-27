ca01788e6d8ae59b823c388624859094e616e255
Chad <cythrawll@codeangel.org>
2021-09-27 13:30:38 -0500

    New: Add decimal formatter to number formatters. (#1480)

--- packages/core/addon/utils/enums/number-formats.ts
+++ packages/core/addon/utils/enums/number-formats.ts
@@ -11,6 +11,7 @@ import { A } from '@ember/array';
 export default A([
   { name: 'Number', format: '0,0.00' },
   { name: 'Decimal', format: '0.000' },
+  { name: 'Percentage', format: '0.00%' },
   { name: 'Nice Number', format: '0.0a' },
   { name: 'Money', format: '$0,0[.]00' },
 ]);
