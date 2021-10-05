f6a2cd5e94ac86b373709d605dbda45a06fa23f9
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-10-05 16:33:22 -0500

    Fix: Include assets from public

--- packages/directory/index.js
+++ packages/directory/index.js
@@ -1,9 +1,19 @@
 'use strict';
+let Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
 
   isDevelopingAddon() {
     return process.env.DEV_NAVI;
+  },
+
+  /**
+   * Override to include this packages `public` dir files
+   */
+  treeForPublic(tree) {
+    const assetsTree = new Funnel('public');
+    return mergeTrees([tree, assetsTree], { overwrite: true });
   },
 };
