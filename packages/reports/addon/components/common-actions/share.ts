0fe88949b1aa766f8529b0d85181a927ea947987
holderb5 <31903532+holderb5@users.noreply.github.com>
2021-11-10 09:47:27 -0600

    Update: Change url getter to an action (#1514)

--- packages/reports/addon/components/common-actions/share.ts
+++ packages/reports/addon/components/common-actions/share.ts
@@ -14,7 +14,8 @@ export default class ShareActionComponent extends Component<Args> {
   @service
   naviNotifications!: NaviNotificationsService;
 
-  get shareUrl() {
+  @action
+  shareUrl() {
     // Allow custom url logic for sharing something other than current page
     const { buildUrl } = this.args;
     return buildUrl ? buildUrl() : document.location.href;
@@ -26,7 +27,7 @@ export default class ShareActionComponent extends Component<Args> {
       title: 'Link copied',
       style: 'info',
       timeout: 'short',
-      extra: this.shareUrl,
+      extra: this.shareUrl(),
     });
   }
 }
