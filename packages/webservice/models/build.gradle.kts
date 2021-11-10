de25d22d86d7305f7f8c3bc2cf805d2bacbaf322
Jon Kilroy <jkilroy@gmail.com>
2021-11-10 09:56:47 -0600

    Update: Upgrade to Elide 6 (#1512)
    * Update: Change `requestVersion` from `val` to `var`
    * Update: Upgrade to Elide 6
    * Update: Add webservice files to .gitignore
    * Build: Bump to version to 0.4.0

--- packages/webservice/models/build.gradle.kts
+++ packages/webservice/models/build.gradle.kts
@@ -23,7 +23,7 @@ allOpen {
 }
 
 dependencies {
-    implementation("com.yahoo.elide", "elide-core", "5.0.12")
+    implementation("com.yahoo.elide", "elide-core", "6.0.1")
     implementation("javax.persistence", "javax.persistence-api", "2.2")
     implementation("org.hibernate", "hibernate-core", "5.4.15.Final")
     implementation("jakarta.mail:jakarta.mail-api:1.6.7")
