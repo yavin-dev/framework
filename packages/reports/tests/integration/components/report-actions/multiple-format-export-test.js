428e6e854f315bccc57ce1e39c1b09d5a444c178
utsabpoud <78504842+utsabpoud@users.noreply.github.com>
2021-12-06 12:21:19 -0500

    Update packages/reports/tests/integration/components/report-actions/multiple-format-export-test.js
    Co-authored-by: Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>

--- packages/reports/tests/integration/components/report-actions/multiple-format-export-test.js
+++ packages/reports/tests/integration/components/report-actions/multiple-format-export-test.js
@@ -92,10 +92,6 @@ module('Integration | Component | report actions - multiple-format-export', func
       const actualModel = this.report.serialize();
       const compressionService = this.owner.lookup('service:compression');
       const decompressedModel = (await compressionService.decompressModel(model)).serialize();
-      /*
-       *  const compressedModel = await compressionService.compressModel(this.report);
-       * assert.equal(model, compressedModel, 'model is serialized');
-       */
       assert.deepEqual(decompressedModel, actualModel, 'model deserializes correctly');
     });
 
