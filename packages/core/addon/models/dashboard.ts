22e1e20c684c751bfe7671bc6e18937c0685d5cf
Jon Kilroy <kilroy@verizonmedia.com>
2021-12-20 13:39:23 -0600

    Fix: ToArray

--- packages/core/addon/models/dashboard.ts
+++ packages/core/addon/models/dashboard.ts
@@ -57,7 +57,7 @@ export default class DashboardModel extends DeliverableItem.extend(Validations) 
   }
 
   get isUserEditor() {
-    return this.editors.some((e: UserModel) => e.id === config.navi.user);
+    return this.editors.toArray().some((e: UserModel) => e.id === config.navi.user);
   }
 
   get canUserEdit(): boolean {
