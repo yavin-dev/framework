5c53acceb94838c04bdfe19606545c2b9ec524b5
Utsav Poudel <utsab.poudel@verizonmedia.com>
2022-01-24 12:22:29 -0500

    Report User component added

---
+++ packages/reports/addon/components/report-user.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2021, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
+import Component from '@glimmer/component';
+import moment from 'moment';
+
+interface ReportUserArgs {
+  user: string;
+  updatedOn: string;
+}
+
+export default class ReportUser extends Component<ReportUserArgs> {
+  get updatedon(): string {
+    return 'Last Modified on ' + moment(this.args.updatedOn).format('MMM');
+  }
+}
