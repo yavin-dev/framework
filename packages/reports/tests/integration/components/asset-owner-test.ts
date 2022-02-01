61de7e39dc3626d31209115299d7c4eeec1b4275
Utsav Poudel <utsab.poudel@verizonmedia.com>
2022-02-01 14:13:32 -0500

    fixed review comments

--- packages/reports/tests/integration/components/asset-owner-test.ts
+++ packages/reports/tests/integration/components/asset-owner-test.ts
@@ -2,23 +2,18 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import moment from 'moment';
 import { ReportUserArgs } from 'navi-reports/components/asset-owner';
 
 module('Integration | Component | asset-owner', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it describes updated user and updated time on reports header', async function (assert) {
-    const now = new Date();
-    const arg: ReportUserArgs = { user: 'test-user', updatedOn: now.toISOString().split('.')[0] };
+    const arg: ReportUserArgs = { user: 'test-user', updatedOn: '2022-01-25T14:32:12' };
     this.set('user', arg.user);
     this.set('updatedOn', arg.updatedOn);
     await render(hbs`<AssetOwner @user={{this.user}} @updatedOn={{this.updatedOn}}/>`);
     assert
       .dom(this.element)
-      .hasText(
-        `${arg.user} - Last Modified on ${moment(arg.updatedOn).format('lll')}`,
-        'it should show updated user and time'
-      );
+      .hasText(`test-user - Last Modified on Jan 25, 2022 2:32 PM`, 'it should show updated user and time');
   });
 });
