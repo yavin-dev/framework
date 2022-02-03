b7c2aecb03f291de29e1bb92105b6ef3c3b6c3b1
utsabpoud <78504842+utsabpoud@users.noreply.github.com>
2022-02-03 16:35:38 -0500

    Update: Asset Owner component (#1555)

---
+++ packages/reports/addon/components/asset-owner.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2022, Yahoo Holdings Inc.
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
+import Component from '@glimmer/component';
+import moment from 'moment';
+
+export interface ReportUserArgs {
+  user: string;
+  updatedOn: string;
+}
+
+export default class ReportUser extends Component<ReportUserArgs> {
+  get updatedon(): string {
+    return 'Last Modified on ' + moment(this.args.updatedOn).format('lll');
+  }
+}
