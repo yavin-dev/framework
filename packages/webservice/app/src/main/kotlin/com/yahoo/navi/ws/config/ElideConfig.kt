1ab04c60df8b4e7c3a9b37f6fc51bfef6362042b
Moiz Arafat <moiz.arafat@verizonmedia.com>
2022-04-29 14:43:59 -0400

    Update: Upgrade to Elide 6.1.5 (#1618)

--- packages/webservice/app/src/main/kotlin/com/yahoo/navi/ws/config/ElideConfig.kt
+++ packages/webservice/app/src/main/kotlin/com/yahoo/navi/ws/config/ElideConfig.kt
@@ -7,10 +7,13 @@ package com.yahoo.navi.ws.config
 
 import com.yahoo.elide.Elide
 import com.yahoo.elide.ElideSettingsBuilder
+import com.yahoo.elide.RefreshableElide
+import com.yahoo.elide.core.TransactionRegistry
 import com.yahoo.elide.core.datastore.DataStore
 import com.yahoo.elide.core.dictionary.EntityDictionary
 import com.yahoo.elide.core.filter.dialect.RSQLFilterDialect
 import com.yahoo.elide.spring.config.ElideConfigProperties
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.TimeZone
@@ -18,7 +21,8 @@ import java.util.TimeZone
 @Configuration
 class ElideConfig {
     @Bean
-    fun initializeElide(dictionary: EntityDictionary, dataStore: DataStore, settings: ElideConfigProperties): Elide {
+    @RefreshScope
+    fun initializeElide(dictionary: EntityDictionary, dataStore: DataStore, settings: ElideConfigProperties): RefreshableElide {
         val builder = ElideSettingsBuilder(dataStore)
             .withEntityDictionary(dictionary)
             .withDefaultMaxPageSize(settings.maxPageSize)
@@ -30,6 +34,6 @@ class ElideConfig {
             .withJsonApiPath(settings.jsonApi.path)
             .withGraphQLApiPath(settings.graphql.path)
             .withExportApiPath(settings.async.export.path)
-        return Elide(builder.build())
+        return RefreshableElide(Elide(builder.build(), TransactionRegistry(), dictionary.getScanner(), true))
     }
 }
