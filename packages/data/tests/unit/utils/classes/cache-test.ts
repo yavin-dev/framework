48d4e06e185f73809e07a8c4a75ba03e743e868b
Miranda Grisa <miranda.grisa@verizonmedia.com>
2022-05-23 15:05:14 -0500

    Fix: More tests

--- packages/data/tests/unit/utils/classes/cache-test.ts
+++ packages/data/tests/unit/utils/classes/cache-test.ts
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 
 module('Unit | Utils | Cache Class', function () {
   test('cache stores and returns records correctly', async function (assert) {
-    assert.expect(6);
+    assert.expect(9);
 
     // setup cache (key : record :: '0' : 'zero')
     const cache = new Cache<string>();
@@ -14,6 +14,15 @@ module('Unit | Utils | Cache Class', function () {
     cache.setItem(cacheId, cacheRecord);
     assert.deepEqual(cache.getItem(cacheId), 'zero', 'checking for values in cache returns values added to cache');
     assert.deepEqual(cache.getItem('1'), undefined, 'checking for values not in cache returns undefined');
+
+    // try removing cache items
+    cache.removeItem(cacheId);
+    assert.notOk(cache.has(cacheId));
+    cache.setItem('0', 'zero');
+    cache.setItem('1', 'one');
+    cache.clear();
+    assert.notOk(cache.has('0'));
+    assert.notOk(cache.has('1'));
 
     // try a complex record type
     const cache2 = new Cache<{ word: string; letters: number }>();
