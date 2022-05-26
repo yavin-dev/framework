122f3d1a2a93412355c137c20e13f7e5e21df79c
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2022-05-26 13:32:35 -0500

    Fix: Revert invalid type imports (#1646)

--- packages/data/addon/serializers/facts/elide.ts
+++ packages/data/addon/serializers/facts/elide.ts
@@ -8,13 +8,9 @@
 import EmberObject from '@ember/object';
 import { getOwner } from '@ember/application';
 import NaviFactSerializer, { ResponseV1 } from './interface';
-import {
-  type AsyncQueryResponse,
-  FactAdapterError,
-  type PageInfo,
-  type RequestOptions,
-} from 'navi-data/adapters/facts/interface';
-import { type RequestV2 } from '@yavin/client/request';
+import { FactAdapterError } from 'navi-data/adapters/facts/interface';
+import type { AsyncQueryResponse, PageInfo, RequestOptions } from 'navi-data/adapters/facts/interface';
+import type { RequestV2 } from '@yavin/client/request';
 import { canonicalizeMetric } from 'navi-data/utils/metric';
 import NaviFactResponse from 'navi-data/models/navi-fact-response';
 import NaviFactError, { NaviErrorDetails } from 'navi-data/errors/navi-adapter-error';
