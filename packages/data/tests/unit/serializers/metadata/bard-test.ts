f8cb9f08762390a43d9bdd8a5e36d1e13e5d1555
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2022-06-08 12:08:02 -0500

    Apply suggestions from code review
    Co-authored-by: Daniel Levinson <daniellevinson@users.noreply.github.com>

--- packages/data/tests/unit/serializers/metadata/bard-test.ts
+++ packages/data/tests/unit/serializers/metadata/bard-test.ts
@@ -18,7 +18,7 @@ import BardTableMetadataModel, { BardTableMetadataPayload } from '@yavin/client/
 import RequestConstraintMetadataModel, {
   RequestConstraintMetadataPayload,
 } from '@yavin/client/models/metadata/request-constraint';
-import { EverythingMetadataPayload } from '@yavin/client/serializers/metadata/interface';
+import type { EverythingMetadataPayload } from '@yavin/client/serializers/metadata/interface';
 import { DataType } from '@yavin/client/models/metadata/function-parameter';
 import { ValueSourceType } from '@yavin/client/models/metadata/elide/dimension';
 
