aa4c574bbb24efa96dabb3c0d2ae36bd2d6ee69d
Kevin Hinterlong <hinterlong@yahooinc.com>
2022-06-21 17:48:08 -0500

    Update: Use @yavin/client injector

--- packages/data/addon/serializers/facts/bard.ts
+++ packages/data/addon/serializers/facts/bard.ts
@@ -11,6 +11,7 @@ import { getOwner } from '@ember/application';
 export default class NaviFiliFactsSerializer extends FiliFactsSerializer {
   static create(args: unknown) {
     const owner = getOwner(args);
-    return new this(owner.lookup('service:client-injector'));
+    const yavinClient = owner.lookup('service:yavin-client');
+    return new this(yavinClient.injector);
   }
 }
