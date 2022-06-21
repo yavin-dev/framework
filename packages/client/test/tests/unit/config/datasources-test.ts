ee6863b74aefa8ceb9a21df3f8ae0384e935ccc3
Kevin Hinterlong <hinterlong@yahooinc.com>
2022-06-21 17:34:28 -0500

    Update: Move Fact service test to client

--- packages/client/test/tests/unit/config/datasources-test.ts
+++ packages/client/test/tests/unit/config/datasources-test.ts
@@ -1,58 +1,8 @@
 import { module, test } from 'qunit';
 import { ClientConfig } from '@yavin/client/config/datasources';
+import { mockConfig } from '../../helpers/config';
 
-const configJson = {
-  dimensionCache: {
-    maxSize: 100,
-    timeoutMs: 10000,
-  },
-  dataSources: [
-    {
-      name: 'bardOne',
-      displayName: 'Bard One',
-      description: 'Interesting User Insights',
-      uri: 'https://data.naviapp.io',
-      type: 'bard',
-      options: {
-        enableDimensionSearch: true,
-        sinceOperatorEndPeriod: 'P3M',
-      },
-    },
-    {
-      name: 'bardTwo',
-      displayName: 'Bard Two',
-      description: 'Awesome Revenue Analytics',
-      uri: 'https://data2.naviapp.com',
-      type: 'bard',
-    },
-    {
-      name: 'elideOne',
-      displayName: 'Elide One',
-      description: 'Elide One Description',
-      uri: 'https://data.naviapp.io/graphql',
-      type: 'elide',
-      namespaces: [
-        {
-          name: 'DemoNamespace',
-          displayName: 'Demo Namespace',
-          description: 'Demo Namespace Description',
-        },
-      ],
-    },
-    {
-      name: 'elideTwo',
-      displayName: 'Elide Two',
-      description: 'Elide Two Description',
-      uri: 'https://data2.naviapp.com/graphql',
-      type: 'elide',
-    },
-  ],
-  defaultDataSource: 'bardOne',
-  cardinalities: {
-    small: 600,
-    medium: 50000,
-  },
-};
+const configJson = mockConfig();
 let clientConfig: ClientConfig;
 
 module('Unit | Config | Config', function (hooks) {
