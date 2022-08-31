0f3b1127f5a7e44f4e0672b46bdbc5e247e5e26c
Kevin Hinterlong <kevinhinterlong@gmail.com>
2022-08-31 15:12:24 -0500

    Fix: Update to exclude namespace when null

--- packages/model-types/lib/models/visualization.d.ts
+++ packages/model-types/lib/models/visualization.d.ts
@@ -2,9 +2,14 @@
  * Copyright 2022, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
-export type Visualization = {
+type BaseVisualization = {
   type: string;
   version: number;
-  namespace: string;
   metadata: Record<string, unknown>;
 };
+
+export type Visualization =
+  | BaseVisualization
+  | (BaseVisualization & {
+      namespace: string;
+    });
